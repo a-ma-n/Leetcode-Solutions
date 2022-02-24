d0c367a801d37b67a26a0fb0d6552bb631e9fc67
Aman Ali (He/Him) <amanali.siddiqui2020@vitstudent.ac.in>
2022-02-24 17:22:14 +0530

    Time: 35 ms (75.35%), Space: 21.5 MB (66.06%) - LeetHub

--- 645-set-mismatch/645-set-mismatch.cpp
+++ 645-set-mismatch/645-set-mismatch.cpp
@@ -1,29 +1,60 @@
 class Solution {
 public:
-    vector<int> findErrorNums(vector<int>& nums) {
+    vector<int> findErrorNums(vector<int>& arr) {
         //find repeated
-        int dup=-1,act_sum=0,expected_sum,n=nums.size(),mis;
-        for(auto it:nums){
-            act_sum+=it;
-        }
-        for(int i=0;i<n;i++){
-            auto val=abs(nums[i]);
-            if(nums[val-1]<0)
-            {   dup=val;
-                break;
-            }
-            
-            nums[val-1]*=-1;
-        }
+         int xor1;
+
+    /* Will have only single set bit of xor1 */
+    int set_bit_no;
+
+    int i;
+    int x = 0; // missing
+    int y = 0; // repeated
+    int n = arr.size();
+
+    xor1 = arr[0];
+
+    /* Get the xor of all array elements */
+    for (i = 1; i < n; i++)
+        xor1 = xor1 ^ arr[i];
+
+    /* XOR the previous result with numbers from 1 to n */
+    for (i = 1; i <= n; i++)
+        xor1 = xor1 ^ i;
+
+    /* Get the rightmost set bit in set_bit_no */
+    set_bit_no = xor1 & ~(xor1 - 1);
+        for (i = 0; i < n; i++) {
+        if (arr[i] & set_bit_no)
+            /* arr[i] belongs to first set */
+            x = x ^ arr[i];
+
+        else
+            /* arr[i] belongs to second set */
+            y = y ^ arr[i];
+    }
+
+    for (i = 1; i <= n; i++) {
+        if (i & set_bit_no)
+            /* i belongs to first set */
+            x = x ^ i;
+
+        else
+            /* i belongs to second set */
+            y = y ^ i;
+    }
+
+    // NB! numbers can be swapped, maybe do a check ?
+    int x_count = 0;
+    for (int i=0; i<n; i++) {
+        if (arr[i]==x)
+            x_count++;
+    }
+    
+    if (x_count==0)
+        return {y, x};
         
-        expected_sum=(n*(n+1))/2;
-        cout<<expected_sum<<endl;
-        cout<<dup<<endl;
-        cout<<act_sum<<endl;
-        mis=expected_sum - act_sum   +dup;
-        vector <int>v;
-        v.push_back(dup);
-        v.push_back(mis);
-        return v;
+        
+        return {x, y};
     }
 };
