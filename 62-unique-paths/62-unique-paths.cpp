2f6f4658ad8568cbc531ae22c7426ff66dd75253
Aman Ali (He/Him) <amanali.siddiqui2020@vitstudent.ac.in>
2022-02-25 18:54:50 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (71.51%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -3,7 +3,7 @@ public:
     int uniquePaths(int m, int n) {
         
         int N = n + m - 2; 
-        int r = m-1;
+        int r = min(m-1,n-1);
         
         double res =1;
         for(int i=1;i<=r;i++){
