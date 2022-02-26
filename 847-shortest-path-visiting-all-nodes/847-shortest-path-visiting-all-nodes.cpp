e7d745c4ba56fb6d29b4133c45a9ac2831a0d5ee
Aman Ali (He/Him) <amanali.siddiqui2020@vitstudent.ac.in>
2022-02-26 10:39:17 +0530

    Time: 298 ms (18.49%), Space: 34.7 MB (9.24%) - LeetHub

---
+++ 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int shortestPathLength(vector<vector<int>>& graph) {
+        // BFS Approach ,TC: O(2^N2),SC=O(n)
+        int i,n=graph.size(),j,ans=0;
+        
+        queue<vector<int>> q;
+        set<pair<int,int>>s;
+        
+        //starting bfs from every node ansd mask is used to check the state of all 
+        // visited nodes
+        for(int i=0;i<n;i++){
+            
+            vector <int> v(3,0);
+            int mask= (1<<i);
+            int node = i;
+            int cost = 0;
+            v[0]=cost;
+            v[1]=node;
+            v[2]=mask;
+            q.push(v);
+            s.insert({node,mask});
+        }
+        
+        //bfs
+        while(q.size()>0){
+            
+            auto z=q.front();
+            q.pop();
+            
+            // all bits are set which means all nodes are finally visited
+            if(z[2]==(1<<n)-1){
+                ans=z[0];
+                break;
+            }
+            
+            for(auto j:graph[z[1]]){
+                
+                int mask =z[2]|(1<<j);
+                
+                //checking the state is not visited yet
+                if(s.find({j,mask})==s.end()){
+                    s.insert({j,mask});
+                    vector<int> v(3,0);
+                    v[0]=z[0]+1;
+                    v[1]=j;
+                    v[2]=mask;
+                    q.push(v);
+                }
+            }
+        }
+        return ans;
+        
+    }
+};
