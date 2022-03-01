ca755eefeb2a85dff1b95fe92899f86c671b839d
Aman Ali (He/Him) <amanali.siddiqui2020@vitstudent.ac.in>
2022-03-01 09:33:25 +0530

    Time: 4 ms (88.16%), Space: 8.3 MB (45.86%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,36 +1,35 @@
 class Solution {
 public:
-    int DecimalToBinary(int num)
-{
-    string str;
-        vector <int>v;
-        int c=0;
-      while(num){
-      if(num & 1) // 1
-       { c++;
-          str+='1';
-       }
-      else // 0
-        str+='0';
-      v.push_back(c);
-      num>>=1; // Right Shift by 1 
-    }   
-      return c;
-}
- 
-// int reverse(string str)
+//     int DecimalToBinary(int num)
 // {
-//   for(int i=str.size()-1 ; i>=0 ; i--)
-//   { 
-//       cout<< str[i];
-//   }
+//     string str;
+//         vector <int>v;
+//         int c=0;
+//       while(num){
+//       if(num & 1) // 1
+//        { c++;
+//           str+='1';
+//        }
+//       else // 0
+//         str+='0';
+//       v.push_back(c);
+//       num>>=1; // Right Shift by 1 
+//     }   
+//       return c;
 // }
-    
+ 
     vector<int> countBits(int n) {
-        vector<int> bitsV ;
-        for(int i=0;i<=n;i++){
-            bitsV.push_back(DecimalToBinary(i));
+        vector<int> res(n);
+        res.push_back(0);//for 0th element
+        if(n==0)
+            return res;
+        
+        for(int i=1;i<=n;i++){
+            if(i%2==0)
+                res[i]=res[i/2];
+            else 
+                res[i]=res[i-1]+1;
         }
-        return bitsV;
+        return res;
     }
 };
