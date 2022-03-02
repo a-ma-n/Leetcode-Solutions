52b8eb76b2b839763e7ca379215c46b9760df4a9
Aman Ali (He/Him) <amanali.siddiqui2020@vitstudent.ac.in>
2022-03-02 09:35:07 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (33.59%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
-    bool isSub(string s, string t, int m,int n){
-        if( m == 0) 
-            return true;
-        if( n == 0 )
-            return false;
-        else{
-            if(s[m-1] == t[n-1]){
-                return  isSub( s,  t,  m-1, n-1);
-            }
-            
-            return  isSub( s,  t,  m, n-1);
-                
-            
-        }
-    }
-    
-        
+
     
     bool isSubsequence(string s, string t) {
         
-       int sizeSubstring = s.length(), sizeString = t.length();
-        // A3 - Recursion
-        // A4 - DP
-        return isSub(s,t,sizeSubstring,sizeString);
-         
+       int sizeSubstring = s.length(), sizeString = t.length(),i,j;
+        unordered_set <char> setLetters;
+        for( i = 0 ; i < sizeSubstring ; i++ ){
+            if( setLetters.find(s[i]) == setLetters.end() )
+                setLetters.insert(s[i]);
+        }
         
+        int countLetters=0;
+        
+        for( i = 0 ; i < sizeString ; i++ ){
+            if( s[countLetters] == t[i] )
+                {
+                // k++;
+                countLetters++;
+            }
+        }
+        cout<<countLetters;
+       if(countLetters == sizeSubstring)
+           return true;
+        else
+            return false;
         
         
         
