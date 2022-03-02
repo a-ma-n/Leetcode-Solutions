9665ed34f3ec4ccf43f15af48dce2527c20b4ee9
Aman Ali (He/Him) <amanali.siddiqui2020@vitstudent.ac.in>
2022-03-02 08:33:44 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (72.95%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -3,27 +3,65 @@ public:
     bool isSubsequence(string s, string t) {
         
         int sizeSubstring = s.length(), sizeString = t.length(),i,j;
-        
-        // A1 -> O(n^2)
-        // A2 -> unordered Hashmap
-        
-        for( i = 0 ; i < sizeSubstring ; ){
-            for( j = 0; j < sizeString ; j++){
-                if( s[i] == t[j] ){
-                    i++;
-                }
-                
-            }
-            if(j==sizeString)
-                break;
-        }
-        
-        if(i==sizeSubstring){
-            return true;
-        }
-        else{
-            return false;
-        }
+        unordered_set <char> setLetters;
        
+        // A2 -> unordered set
+        
+        for( i = 0 ; i < sizeSubstring ; i++ ){
+            if( setLetters.find(s[i]) == setLetters.end() )
+                setLetters.insert(s[i]);
+        }
+        
+        int countLetters=0;
+        
+        for( i = 0 ; i < sizeString ; i++ ){
+            if( s[countLetters] == t[i] )
+                {
+                // k++;
+                countLetters++;
+            }
+        }
+        cout<<countLetters;
+       if(countLetters == sizeSubstring)
+           return true;
+        else
+            return false;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// A1 -> O(n^2)
+//         for( i = 0 ; i < sizeSubstring ; ){
+//             for( j = 0; j < sizeString ; j++){
+//                 if( s[i] == t[j] ){
+//                     i++;
+//                 }
+                
+//             }
+//             if(j==sizeString)
+//                 break;
+//         }
+        
+//         if(i==sizeSubstring){
+//             return true;
+//         }
+//         else{
+//             return false;
+//         }
