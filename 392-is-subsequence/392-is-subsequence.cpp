ae5c23cf90e2af87023e0d7c8cb0ea0fcb3a7bf9
Aman Ali (He/Him) <amanali.siddiqui2020@vitstudent.ac.in>
2022-03-02 09:48:13 +0530

    Time: 8 ms (15.48%), Space: 6.8 MB (19.62%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,28 +1,48 @@
 class Solution {
 public:
-
     
-    bool isSubsequence(string s, string t) {
-        
-       int sizeSubstring = s.length(), sizeString = t.length(),i,countLetters=0;
-       unordered_set <char> setLetters;
-        
-        for( i = 0 ; i < sizeSubstring ; i++ ){
-            if( setLetters.find(s[i]) == setLetters.end() )
-                setLetters.insert(s[i]);
+     int helper(string x, string y) {
+        int m = x.size();
+        int n = y.size();
+        int dp[m+1][n+1];
+         
+        for(int i = 0; i<=m; i++)
+        {
+            dp[i][0]=0;
         }
-        for( i = 0 ; i < sizeString ; i++ ){
-            if( s[countLetters] == t[i] )
-                {
-                countLetters++;
+         
+        for(int i = 0; i<=n; i++)
+        {
+            dp[0][i]=0;
+        }
+        
+        for(int i = 1; i<=m; i++){
+            for(int j = 1; j<=n; j++){
+                if(x[i-1]==y[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
             }
         }
-       if(countLetters == sizeSubstring)
-           return true;
-        else
-            return false;
+        return dp[m][n];
+    }
+    
+    
+    bool isSubsequence(string smaller, string larger) {
+        
+       int x  = helper(smaller,larger);
+        if(x == smaller.size()){
+            return true;
+        }
+        return false;     
     }
 };
+
+
+
+
 
 
 
