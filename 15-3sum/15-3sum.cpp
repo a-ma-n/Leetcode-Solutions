82500b9cc1879bd86f94e0ab6284c53408dc06f2
Aman Ali (He/Him) <amanali.siddiqui2020@vitstudent.ac.in>
2022-03-12 22:43:29 +0530

    Time: 190 ms (30.79%), Space: 23.5 MB (32.23%) - LeetHub

---
+++ 15-3sum/15-3sum.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& num) {
+       
+        //int i,left,right;
+       
+        vector <vector <int> > res;
+        
+        sort(num.begin(),num.end());
+        
+        for(int i=0;i<(int)(num.size()-2);i++){
+            
+            if(i==0 || (i>0 && num[i] !=num[i-1])){
+                
+                int lo = i+1,hi = num.size()-1,sum = 0 -num[i];
+                
+                while(lo<hi){
+                    if (num[lo] + num[hi] == sum){
+                        
+                        vector <int> temp;
+                        temp.push_back(num[i]);
+                        temp.push_back(num[lo]);
+                        temp.push_back(num[hi]);
+                        res.push_back(temp);
+                        
+                        while (lo<hi && num[lo] == num[lo+1]) lo++; // move it to next digit
+                        while (lo<hi && num[hi] == num[hi-1]) hi--;// move it to next unique digit
+                        
+                        lo++;hi--;
+                        
+                    }
+                    
+                
+                else if (num[lo]+num[hi] < sum) lo++;
+                else hi--;
+                }
+            }
+        }
+        return res;
+        
+    }
+};
