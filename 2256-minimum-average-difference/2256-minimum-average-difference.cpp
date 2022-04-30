e60e3863ab507214d326a62569940d19110e2e97
Aman Ali (He/Him) <amanali.siddiqui2020@vitstudent.ac.in>
2022-04-30 22:41:29 +0530

    Time: 194 ms (33.33%), Space: 78.3 MB (100.00%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.cpp
+++ 2256-minimum-average-difference/2256-minimum-average-difference.cpp
@@ -1,34 +1,38 @@
 class Solution {
 public:
     int minimumAverageDifference(vector<int>& nums) {
-      int n=nums.size();
-        
-        long long right_sum = 0;
-        for(auto it:nums) right_sum += it;
-        
-        int mini = INT_MAX;
-        int idx = 0;
-        
-        long long left_sum = 0;
-        
-        for(int i=0; i<n; i++)
-        {
-            left_sum += nums[i];
-            right_sum -= nums[i];
-            
-            long long first = (left_sum/(i+1));
-    
-            long long last = i<n-1 ? (right_sum/(n-i-1)) : 0;
-            
-            int diff = abs(first - last);
-            
-            if(diff < mini)
-            {
-                mini = diff;
-                idx = i;
-            }
+        //vector<int> diff;
+        long long avg,i,j,s1=0,s2=0,n1,n2,n=nums.size(),min=0,pos=0,sum=0,val1=0,val2=0,diff=0;
+        for(i=0;i<n;i++){
+            sum+=nums[i];
         }
-        
-        return idx;
+        //cout<<sum<<endl;
+        //diff.resize(n);
+        for(i=0;i<n;i++){
+            avg=0;
+            s1+=nums[i];
+            n1=i+1;
+            //n2=n-i-1;
+            val1=floor(s1/n1);
+            s2=sum-s1;
+            n2=n-1-i;
+            if(n2!=0)
+            {   
+                val2=floor(s2/n2);
+                diff=abs(val1-val2);
+                if(i==0)
+                    min=diff;
+            }
+            else{
+                diff=abs(val1);
+                cout<<"\ndiff:"<<diff<<endl;
+            }
+            if(min>diff){
+                min = diff;
+                pos=i;
+            }            
+            //cout<<"i: "<<i<<" s1:"<<s1<<" s2:"<<s2<<" n1:"<<n1<<" n2:"<<n2<<" val1:"<<val1<<" val2:"<<val2<<" diff "<<diff[i]<<endl;
+        }
+        return pos;
     }
 };
