1ca608f6a245500fa435e733a57bca3c35e86d8e
Aman Ali (He/Him) <amanali.siddiqui2020@vitstudent.ac.in>
2022-05-01 13:42:18 +0530

    Time: 41 ms (65.88%), Space: 19.7 MB (91.70%) - LeetHub

---
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.cpp
@@ -0,0 +1,21 @@
+class KthLargest {
+public:
+    priority_queue<int,vector<int>, greater<int>>minHeap;
+    int size;
+    KthLargest(int k, vector<int>& nums) {
+        size=k;
+        for(int ele : nums) add(ele);
+    }
+    
+    int add(int val) {
+        if(minHeap.size() < size)
+            minHeap.push(val);
+        else{
+            if(val > minHeap.top()){
+                minHeap.pop();
+                minHeap.push(val);
+            }
+        }
+        return minHeap.top();
+    }
+};
