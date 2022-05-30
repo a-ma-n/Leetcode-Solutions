8d6982ccf923a0b0a8d78e7c3ca0c83dc671b6fe
Aman Ali (He/Him) <amanali.siddiqui2020@vitstudent.ac.in>
2022-05-31 00:16:14 +0530

    Time: 34 ms (5.35%), Space: 6 MB (42.33%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -2,10 +2,6 @@ class Solution {
 public:
     
     int fib(int n) {
-        if(n==0)
-            return 0;
-        if(n==1)
-            return 1;
-        return fib(n-1)+fib(n-2);
+        return (n==0)?0:(n==1)?1:fib(n-1)+fib(n-2);
     }
 };
