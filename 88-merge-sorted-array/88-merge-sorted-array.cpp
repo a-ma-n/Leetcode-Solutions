c08adff12e3be475d062442d3659a49e4c543408
Aman Ali (He/Him) <amanali.siddiqui2020@vitstudent.ac.in>
2022-06-07 09:47:34 +0530

    Time: 5 ms (34.50%), Space: 9.1 MB (72.57%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        
-        for(int i=m;i<nums1.size();i++){
-            nums1[i]=nums2[i-m];
+        void merge(vector<int>& numbers1, int size1, vector<int>& numbers2, int size2)
+    {                        
+        int index1 = size1 - 1, index2 = size2 - 1, validIndex = numbers1.size() - 1;
+        while (index1 >= 0 && index2 >= 0)
+        {
+            numbers1[validIndex--] = (numbers1[index1] > numbers2[index2]) 
+                                     ? (numbers1[index1--]) : (numbers2[index2--]);
         }
-        sort(nums1.begin(),nums1.end());
+        
+        while (index2 >= 0) numbers1[validIndex--] = numbers2[index2--];
     }
 };
