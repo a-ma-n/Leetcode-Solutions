a67fafd4830a94710b13e607736f39a7bc1b08a1
Aman Ali (He/Him) <amanali.siddiqui2020@vitstudent.ac.in>
2022-06-14 17:39:21 +0530

    Time: 7 ms (7.68%), Space: 6 MB (94.06%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.cpp
+++ 165-compare-version-numbers/165-compare-version-numbers.cpp
@@ -5,39 +5,46 @@ public:
     int n1=v1.size(),n2=v2.size();
         
         for(int i=0,j=0;i<n1 || j<n2;++i,++j){
+            
             string s1="",s2="";
+            
+            
             while(i<n1 && v1[i]!='.')
             {
+                // remove all leading zeroes
                 if(s1.size() == 0 && v1[i]=='0'){
                     ++i;
                     continue;
                 }
+                // add the version after removing zeroes
                 s1+=v1[i++];
             }
-            while(j<n2 && v2[j]!='.'){
+            
+            while(j<n2 && v2[j]!='.')
+            {
+                // remove all leading zeroes
                 if(s2.size()==0 && v2[j]=='0'){
                     ++j;
                     continue;
                 }
+                // add the version after removing zeroes
                 s2+=v2[j++];
             }
             
+            
             if(s1.size()<s2.size())
                 return -1;
+            
             else if(s2.size()<s1.size())
                 return 1;
             
             ans=s1.compare(s2);
+            
             if(ans<0)
                 return -1;
             else if(ans>0)
                 return 1;
-        
-            
         }
-        
         return 0;
-
-        
     }
 };
