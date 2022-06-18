2a67f80dddccc4684f5a2abdc04c6c1c4dfa7705
Aman Ali (He/Him) <amanali.siddiqui2020@vitstudent.ac.in>
2022-06-18 15:49:43 +0530

    Time: 20 ms (79.93%), Space: 26.3 MB (60.11%) - LeetHub

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -11,28 +11,24 @@
  */
 class Solution {
 public:
-    TreeNode* rec(int l, int r) {
-        if (l > r) return NULL;
-        
-        int i = 0;
-        while (m_inorder[i] != m_postorder[m_curr]) {
-            i++;
-        }
-        
-        m_curr--;
-        TreeNode* node = new TreeNode(m_inorder[i]);
-        node->right = rec(i+1, r);
-        node->left = rec(l, i-1);
-        return node;
-        
-    }
-    
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        m_postorder = postorder, m_inorder = inorder, m_curr = inorder.size()-1;
-        return rec(0, postorder.size()-1);
+         if (inorder.size() != postorder.size())
+            return NULL;
+        map<int,int> hm;
+        for (int i=0;i<inorder.size();++i)
+            hm[inorder[i]] = i;
+        return buildTreePostIn(inorder, 0, inorder.size()-1, postorder, 0, 
+                              postorder.size()-1,hm);
     }
-    
-private:
-    int m_curr;
-    vector<int> m_postorder, m_inorder;
+    TreeNode* buildTreePostIn(vector<int> &inorder, int is, int ie, vector<int> &postorder, int ps, int pe, 
+                                     map<int,int> &hm){
+        if (ps>pe || is>ie) return NULL;
+        TreeNode* root = new TreeNode(postorder[pe]);
+        int ri = hm[postorder[pe]];
+        TreeNode* leftchild = buildTreePostIn(inorder, is, ri-1, postorder, ps, ps+ri-is-1, hm);
+        TreeNode* rightchild = buildTreePostIn(inorder,ri+1, ie, postorder, ps+ri-is, pe-1, hm);
+        root->left = leftchild;
+        root->right = rightchild;
+        return root;
+    }
 };
