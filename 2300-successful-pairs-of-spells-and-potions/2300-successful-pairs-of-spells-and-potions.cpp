972f5ab1c2846106a1ef364f51236a5ae2ebb497
Aman Ali (He/Him) <amanali.siddiqui2020@vitstudent.ac.in>
2022-06-19 07:57:57 +0530

    Time: 275 ms (92.24%), Space: 100 MB (26.35%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
-    vector<int> successfulPairs(vector<int>& s, vector<int>& p, long long target) {
-       
-        sort(p.begin(), p.end());
-        
-        int n = s.size();
-        int m = p.size();
-        
-        vector<int> v1;
-        
-        for(int i=0; i<n; i++){
-            
-            // req number
-            long long req = target/s[i] + (target%s[i] != 0);
-            auto it = lower_bound(p.begin(), p.end(), req);
-            
-            if(it == p.end())     v1.push_back(0);
-            else{
-                int ind = it - p.begin();
-                v1.push_back(m-ind);
-            }
+     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        sort(potions.begin(), potions.end());
+        vector<int> res;
+        for (int a: spells) {
+            long need = (success + a - 1) / a;
+            auto it = lower_bound(potions.begin(), potions.end(), need);
+            res.push_back(potions.end() - it);
         }
-        return v1;}
+        return res;
+    }
+
         
 };
