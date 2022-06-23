ea215f6e23d9cd8eb3f5ae174579bd60f8653e8e
Aman Ali (He/Him) <amanali.siddiqui2020@vitstudent.ac.in>
2022-06-23 18:39:24 +0530

    Time: 35 ms (42.20%), Space: 14.3 MB (42.74%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -10,13 +10,14 @@ public:
                     return true;
             }
         }
+      // if we visit a node twice mark it 2 means a cycle exists
         vis[id] = 2;
         return false;
     }
     bool canFinish(int n, vector<vector<int>>& pre) {
         vector<int> adj[n];
-        for(auto edge : pre)
-            adj[edge[1]].push_back(edge[0]);
+        for(auto edge : pre)//passing the pre requisite
+            adj[edge[1]].push_back(edge[0]);// at index of pre req add new element
         vector<int> vis(n,0);
         
         for(int i=0;i<n;i++){
