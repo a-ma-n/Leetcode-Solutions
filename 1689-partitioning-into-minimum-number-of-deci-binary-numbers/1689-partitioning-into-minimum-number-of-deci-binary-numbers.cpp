41ad6f85082e5a7962616e83be81fdcea90aa59a
Aman Ali (He/Him) <amanali.siddiqui2020@vitstudent.ac.in>
2022-06-27 12:18:52 +0530

    Time: 45 ms (47.99%), Space: 13.4 MB (92.75%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     int minPartitions(string n) {
-        int m=n.size();
-        sort(n.begin(),n.end());
-        m=n[m-1];
-        return (m-48);
+        return (int)*max_element(n.begin(), n.end())-48;
     }
 };
