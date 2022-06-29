7c729c348b4f69b15f443b857ecf3ef681c603af
Aman Ali (He/Him) <amanali.siddiqui2020@vitstudent.ac.in>
2022-06-29 19:11:26 +0530

    Time: 20 ms (70.96%), Space: 10.3 MB (87.22%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,10 +1,27 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& A) {
-        int len = 0;
-        for(auto cur : A) 
-            if(len == 0 || A[len-1] < cur) A[len++] = cur;             // extend
-            else *lower_bound(begin(A), begin(A) + len, cur) = cur;    // replace
-        return len;
+    int lengthOfLIS(vector<int>& arr) {
+        int n = arr.size();
+        int dp[n]; // dp[i] represents i+1'th length LIS ending at minimum integer dp[i]
+        int ans = 0;
+        
+        for (int i = 0; i < n; i++)
+        {
+        /*
+            Since dp array stores elements in the sorted order therefore
+            we can use binary search to find the correct position for
+            arr[i] to be placed.
+            And elements are present in the dp array from 0 to ans-1 position
+            So we will be doing the binary search in this range.
+        */
+        int position = lower_bound(dp, dp + ans, arr[i]) - dp;
+        dp[position] = arr[i];
+
+            if (position == ans)
+            {
+                ans++;
+            }
+        }
+    return ans;
     }
 };
