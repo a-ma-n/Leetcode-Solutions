70f4dad3f55e343ac256c7764884723959844d54
Aman Ali (He/Him) <amanali.siddiqui2020@vitstudent.ac.in>
2022-07-08 00:47:09 +0530

    Time: 1 ms (62.72%), Space: 6.2 MB (9.16%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
-    int f(int n,vector<int> &dp){
-        if(dp[n]!=-1) return dp[n];
-        if(n==0) return dp[n]=0;
-        else if(n==1) return dp[n]=1;
-        else{
-            return dp[n]=f(n-1,dp)+f(n-2,dp);
-        } 
-       // return (n==0)?0:(n==1)?1:fib(n-1)+fib(n-2);
-    }
+    // int f(int n,vector<int> &dp){
+    //     if(dp[n]!=-1) return dp[n];
+    //     if(n==0) return dp[n]=0;
+    //     else if(n==1) return dp[n]=1;
+    //     else{
+    //         return dp[n]=f(n-1,dp)+f(n-2,dp);
+    //     } 
+    //    // return (n==0)?0:(n==1)?1:fib(n-1)+fib(n-2);
+    // }
     int fib(int n) {
+        if(n==0 || n==1) return n;
         vector<int> dp(n+1,-1);
-        return f(n,dp);
+        dp[0]=0;
+        dp[1]=1;
+        for(int i = 2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
