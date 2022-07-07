2e18df667e0f2386667dbcce60132147528b34a9
Aman Ali (He/Him) <amanali.siddiqui2020@vitstudent.ac.in>
2022-07-08 00:04:21 +0530

    Time: 6 ms (38.54%), Space: 5.9 MB (40.84%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,7 +1,16 @@
 class Solution {
 public:
-    
+    int f(int n,vector<int> &dp){
+        if(dp[n]!=-1) return dp[n];
+        if(n==0) return dp[n]=0;
+        else if(n==1) return dp[n]=1;
+        else{
+            return dp[n]=f(n-1,dp)+f(n-2,dp);
+        } 
+       // return (n==0)?0:(n==1)?1:fib(n-1)+fib(n-2);
+    }
     int fib(int n) {
-        return (n==0)?0:(n==1)?1:fib(n-1)+fib(n-2);
+        vector<int> dp(n+1,-1);
+        return f(n,dp);
     }
 };
