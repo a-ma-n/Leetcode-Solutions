08fd0a64e9d1a7253731caaccd9c098450930926
Aman Ali (He/Him) <amanali.siddiqui2020@vitstudent.ac.in>
2022-07-11 13:07:12 +0530

    Time: 12 ms (10.18%), Space: 12.8 MB (6.59%) - LeetHub

---
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+// level order traversal's last element at each lelve
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        queue <pair<TreeNode*,int>> q;
+        
+       // vector<int> res;
+        map<int,vector<int>> mpp;
+        vector<vector<int>> ans;
+        if(root == NULL) 
+            return ans;
+        
+        q.push({root,0});
+        
+        while(!q.empty()){
+            TreeNode* node = q.front().first;
+            int level = q.front().second;
+            cout<<level;
+            q.pop();
+            
+            if(node->left!=NULL)
+                 q.push({node->left,level+1});
+             if(node->right!=NULL)
+                 q.push({node->right,level+1});
+            mpp[level].push_back(node->val);
+        }
+        
+        for(auto it:mpp){
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+    vector<int> rightSideView(TreeNode* root) {
+        vector<vector<int>> res;
+        vector<int> ans;
+        if(!root) return ans;
+        res = levelOrder(root);
+        for(auto it: res){
+            ans.push_back(it[it.size()-1]);
+        }
+        return ans;
+    }
+};
