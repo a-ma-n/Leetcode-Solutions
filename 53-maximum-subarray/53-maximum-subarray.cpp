a7d8eee8771bc45ef034d4c7e97654a6d7f2ee53
Aman Ali (He/Him) <amanali.siddiqui2020@vitstudent.ac.in>
2022-07-14 12:43:07 +0530

    Time: 184 ms (37.12%), Space: 73.9 MB (5.93%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        int res = 0;
-        maxSubArray(nums, nums.size() - 1, res);
-        return res;
-    }
-    
-    int maxSubArray(vector<int>& nums, size_t n, int& max) {
-        if (n == 0) { max = nums[0]; return nums[0]; }
-        int soFar = maxSubArray(nums, n - 1, max);
-        soFar = std::max(soFar + nums[n], nums[n]);
-        max = std::max(max, soFar);
+    int recur(vector<int>&nums,size_t n,int &max){
+         if (n == 0) { max = nums[0]; return nums[0]; }
+        int soFar = recur(nums,n-1,max);
+        soFar = std::max(soFar+nums[n],nums[n]);// either we consider or we take the element we are standing at
+        max = std::max(max,soFar);
         return soFar;
     }
+    
+    
+    int maxSubArray(vector<int>& nums) {
+        int res=0;
+        //if(nums.size()==1) return nums[0];
+        recur(nums,nums.size()-1,res);
+        return res;
+    }
 };
+
+    
+    // int maxSubArray(vector<int>& nums, size_t n, int& max) {
+    //     if (n == 0) { max = nums[0]; return nums[0]; }
+    //     int soFar = maxSubArray(nums, n - 1, max);
+    //     soFar = std::max(soFar + nums[n], nums[n]);
+    //     max = std::max(max, soFar);
+    //     return soFar;
+    // }
