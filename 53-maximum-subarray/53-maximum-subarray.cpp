d9d0bee61e443904ec4e3ebbb8215ad83a791072
Aman Ali (He/Him) <amanali.siddiqui2020@vitstudent.ac.in>
2022-07-14 12:40:28 +0530

    Time: 198 ms (27.61%), Space: 73.9 MB (5.93%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int sum=0,maxSum=nums[0];
-            for(auto it:nums){
-                
-                sum+=it;
-                maxSum=max(maxSum,sum);
-                if(sum<0){
-                    sum=0;
-                }
-                
-            }
-        return maxSum;
+        int res = 0;
+        maxSubArray(nums, nums.size() - 1, res);
+        return res;
+    }
+    
+    int maxSubArray(vector<int>& nums, size_t n, int& max) {
+        if (n == 0) { max = nums[0]; return nums[0]; }
+        int soFar = maxSubArray(nums, n - 1, max);
+        soFar = std::max(soFar + nums[n], nums[n]);
+        max = std::max(max, soFar);
+        return soFar;
     }
 };
