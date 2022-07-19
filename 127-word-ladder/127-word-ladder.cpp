ba65cce11ee1f0d84dc6623d49537d54543e2f87
Aman Ali (He/Him) <amanali.siddiqui2020@vitstudent.ac.in>
2022-07-19 11:01:11 +0530

    Time: 870 ms (32.33%), Space: 15.9 MB (41.49%) - LeetHub

--- 127-word-ladder/127-word-ladder.cpp
+++ 127-word-ladder/127-word-ladder.cpp
@@ -5,34 +5,48 @@ public:
         queue<string> q;
       
         //hashset
-        set<string> st,words;
+        set<string> visited,words;
         
         // convert to a set
         for(auto it:wordList)
             words.insert(it);
         
         q.push(beginWord);
-        st.insert(beginWord);
+        visited.insert(beginWord);
         
         q.push(beginWord);
-        int res=0; // stores the count of the changes
+        int changes=0; // stores the count of the changes
         
         while(q.size()){
             int n = q.size();
-            res++;
+            // increment the changes variable
+            changes++;
+            
             for(int i =0;i<n;i++){
+                // bfs using the queue
                 string s = q.front();
+                
                 if(s==endWord)
-                    return res;
+                    return changes;
+                
                 q.pop();
+                
                 string t;
+                
+                // trying out variations at different positions
                 for(int i =0; i<s.size() ; i++){
                     t=s;
+                    // trying out variations a-z at that place
                     for( int j = 'a' ; j<='z' ; j++ ){
+                        
                         t[i]=j;
-                        if(words.count(t) && !st.count(t) ){
+                        
+                        if(words.count(t) && !visited.count(t) ){
+                            // if in the wordList and not in the visited list
+                            
                             q.push(t);
-                            st.insert(t);
+                            
+                            visited.insert(t);
                         }
                     }
                 }
