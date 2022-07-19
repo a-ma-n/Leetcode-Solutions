a90a59829bdd20532976458827a68e19a0dc077e
Aman Ali (He/Him) <amanali.siddiqui2020@vitstudent.ac.in>
2022-07-19 22:49:27 +0530

    Time: 12 ms (94.59%), Space: 13.3 MB (52.88%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        bool top=false;
+        bool right=false;
+        int rows=matrix.size(),cols=matrix[0].size();
+        
+        if( rows==1 && cols ==1 ) return;
+        
+        for(int i = 0 ; i<cols;i++){
+            // topmost col check
+            if(matrix[0][i]==0){ 
+                top=true;
+                break;
+            }
+        }
+        //  cout<<matrix[0][cols-1];
+        // cout<<matrix[1][cols-1];
+        // cout<<matrix[2][cols-1];
+        // cout<<rows;
+        for(int i = 0 ; i<rows;i++)
+            {
+            // rightmost col check
+            if(matrix[i][cols-1]==0) {
+                 right=true;
+                // break;
+            }
+        }
+        
+        for(int i=1;i<rows;i++){
+            for(int j=0;j<cols-1;j++){
+                if(matrix[i][j]==0){
+                    matrix[0][j]=0;
+                    matrix[i][cols-1]=0;
+                }
+            }
+        }
+        
+        for(int i=rows-1;i>0;i--){
+            for(int j = cols-2;j>=0;j--){
+                if(matrix[0][j]==0) matrix[i][j]=0;
+                if(matrix[i][cols-1]==0) matrix[i][j]=0;
+            }
+        }
+        // loop to set top row and rightmost col 0 if there was originally a zero there
+        if(top){
+            for(int i = 0 ; i < cols ; i++ ){
+                matrix[0][i]=0;
+            }
+        }
+        if(right){
+            for(int i = 0 ; i < rows ; i++ ){
+                matrix[i][cols-1]=0;
+            }
+        }
+        //cout<<top0<<endl<<right0;
+        
+    }
+};
