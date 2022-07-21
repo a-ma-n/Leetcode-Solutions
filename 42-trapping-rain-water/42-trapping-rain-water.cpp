debd3b222c459a8a09cd6518de29dbb31d7c364b
Aman Ali (He/Him) <amanali.siddiqui2020@vitstudent.ac.in>
2022-07-21 22:11:51 +0530

    Time: 26 ms (45.39%), Space: 19.7 MB (75.82%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -4,13 +4,17 @@ public:
         int left =0, right = height.size()-1;
         int ans = 0;
         int left_max = 0, right_max = 0;
-        while(left<right){
-            if(height[left] < height[right]){
+        while(left<right){ // till we reach the middle (or any point between the outer 2 limits)
+            if(height[left] <= height[right]){
+                // while height of left is less than right height, store the max left height and store the trapped rainwater
                 height[left]>=left_max?(left_max = height[left])  : ans+=(left_max-height[left]);
+                // keep moving left index
                 ++left;
             }
             else{
+                //
                 height[right]>=right_max?(right_max = height[right]):ans += (right_max-height[right]);
+                // move the right index backwards
                 --right;
             }
         }
