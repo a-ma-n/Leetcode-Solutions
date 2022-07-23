20ce8f5b7c6299eafa01cfbb5e24d574949710f0
Aman Ali (He/Him) <amanali.siddiqui2020@vitstudent.ac.in>
2022-07-23 18:12:03 +0530

    Time: 81 ms (19.88%), Space: 28.9 MB (5.57%) - LeetHub

---
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    long change(int T, vector<int>& arr) {
+    
+    int n = arr.size();
+    vector<long> prev(T+1,0);
+    
+    
+    //Initializing base condition
+    for(int i=0;i<=T;i++){
+        if(i%arr[0]==0)
+            prev[i]=1;
+        // Else condition is automatically fulfilled,
+        // as prev array is initialized to zero
+    }
+    
+    for(int ind=1; ind<n;ind++){
+        vector<long> cur(T+1,0);
+        for(int target=0;target<=T;target++){
+            long notTaken = prev[target];
+            
+            long taken = 0;
+            if(arr[ind]<=target)
+                taken = cur[target-arr[ind]];
+                
+            cur[target] = notTaken + taken;
+        }
+        prev = cur;
+    }
+    
+    return prev[T];
+        
+
+    }
+};
