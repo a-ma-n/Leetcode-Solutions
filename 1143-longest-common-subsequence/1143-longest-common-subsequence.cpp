4b819daed20bf9ef72a2f40d591bd57a3b38aaf4
Aman Ali (He/Him) <amanali.siddiqui2020@vitstudent.ac.in>
2022-07-25 08:55:56 +0530

    Time: 19 ms (79.55%), Space: 6.4 MB (98.50%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -21,25 +21,3 @@ public:
         return prev[m];
     }
 };
-
-/*
-
-    vector<int> prev(m+1,0), cur(m+1,0);
-    
-    // Base Case is covered as we have initialized the prev and cur to 0.
-    
-    for(int ind1=1;ind1<=n;ind1++){
-        for(int ind2=1;ind2<=m;ind2++){
-            if(s1[ind1-1]==s2[ind2-1])
-                cur[ind2] = 1 + prev[ind2-1];
-            else
-                cur[ind2] = 0 + max(prev[ind2],cur[ind2-1]);
-        }
-        prev= cur;
-    }
-    
-    return prev[m];
-}
-
-
-*/
