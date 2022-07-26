ef997be5648ac72c44be77c6fac4053d0641d9bd
Aman Ali (He/Him) <amanali.siddiqui2020@vitstudent.ac.in>
2022-07-26 10:11:33 +0530

    Time: 450 ms (5.06%), Space: 98.3 MB (96.58%) - LeetHub

---
+++ 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxDistance(vector<int>& nums1, vector<int>& nums2) {
+        reverse(nums2.begin(),nums2.end());
+        int ans =0;
+        for(int i = 0 ;i<nums1.size();i++){
+            // Find the first element greater than or equal to nums[i]
+            auto it = lower_bound(nums2.begin(),nums2.end(),nums1[i])-nums2.begin();
+            // index of the found element in the original array
+            int j = (nums2.size()-1)-it;
+            if(i<=j) ans = max(ans,j-i); 
+        }
+        return ans;
+    }
+};
