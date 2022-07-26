2d4f197b07b5efda2152ee217a7ac1e15f66a6c2
Aman Ali (He/Him) <amanali.siddiqui2020@vitstudent.ac.in>
2022-07-26 20:43:11 +0530

    Time: 19 ms (32.33%), Space: 14.3 MB (26.16%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -2,48 +2,28 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int x, int y, int newColor) {
-         int oldColor = image[x][y];
-
-    // Number of rows.
-    int m = image.size();
-
-    // Number of columns.
-    int n = image[0].size();
-
-    // Queue to hold the coordinates of the pixels.
-    queue<pair<int, int>> q;
-
-    q.push({x, y});
-
-    while (!q.empty())
-        {
-        pair<int, int> currentPixel = q.front();
-        q.pop();
-
-        // i and j represent the row and column of the current pixel.
-        int i = currentPixel.first;
-        int j = currentPixel.second;
-
-
-        // Check if the current coordinates are valid.
-        if (i >= 0 && i < m && j >= 0 && j < n)
-        {
-            // Now, check if the current pixel has been colored or not.
-            if (image[i][j] == oldColor && image[i][j] != newColor)
-            {
-                // So, replace the old colour.
-                image[i][j] = newColor;
-
-                // Push the adjacent pixels into the queue.
-                q.push({i, j + 1});
-                q.push({i, j - 1});
-                q.push({i + 1, j});
-                q.push({i - 1, j});
+        int oldColor  = image[x][y],i,j ;
+        int m = image.size(),n=image[0].size();
+        queue<pair<int,int>> q;
+        q.push({x,y});
+        while(!q.empty()){
+            auto pixel = q.front();
+            i = pixel.first;
+            j = pixel.second;
+            q.pop();
+            
+            if( i<m && j<n && i>=0 && j>=0){
+                if (image[i][j] == oldColor && image[i][j] != newColor)
+                {
+                image[i][j]=newColor;
+                q.push({i+1,j});
+                q.push({i-1,j});
+                q.push({i,j+1});
+                q.push({i,j-1});
+                }
             }
         }
-    }
-
-    return image;
+        return image;
 }
         
     
