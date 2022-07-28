bac43d3d0a9ef793eb6a299bd3407e1f656652bd
Aman Ali (He/Him) <amanali.siddiqui2020@vitstudent.ac.in>
2022-07-28 12:09:28 +0530

    Time: 117 ms (54.64%), Space: 30.4 MB (39.05%) - LeetHub

---
+++ 542-01-matrix/542-01-matrix.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    /*
+    Complexity Analysis:
+    
+    Time complexity: O(r.c)
+    Space complexity: O(r.c)
+    An additional O(r⋅c) space is required to maintain the queue.
+    */
+    // BFS Solution
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int rows=mat.size();
+        if(!rows) return mat; // rows==0
+        int cols = mat[0].size();
+        vector<vector<int>> dist(rows,vector<int> (cols,INT_MAX));
+        queue<pair<int,int>> q;
+        
+        for(int i = 0 ;i <rows ; i++)
+            for(int j = 0 ; j< cols;j++)
+                if(!mat[i][j]){
+                    dist[i][j]=0;
+                    q.push({i,j}); // Put all o's in the queue
+                }
+        
+        int dir[4][2] = {{-1,0},{1,0},{0,-1},{0,1}};
+        while(!q.empty()){
+            pair<int,int> curr = q.front();
+            q.pop();
+            for(int k=0;k<4;k++){
+                int new_r = curr.first+dir[k][0],new_c = curr.second+dir[k][1];
+                
+            if(new_r>=0 && new_c>=0 && new_r<rows && new_c < cols){
+                    if(dist[new_r][new_c] > dist[curr.first][curr.second] + 1){
+                        dist[new_r][new_c] = dist[curr.first][curr.second]+1;
+                                                            q.push({new_r,new_c});
+                    
+                }
+            }
+        }     
+        }
+             return dist;       
+        
+    }
+};
