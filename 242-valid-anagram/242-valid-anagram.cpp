3ca66d03f4ab887c9fcbc7a76fd1a3555ce16f45
Aman Ali (He/Him) <amanali.siddiqui2020@vitstudent.ac.in>
2022-07-28 09:23:46 +0530

    Time: 21 ms (28.88%), Space: 7.5 MB (7.79%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,35 +1,13 @@
-#define NO_OF_CHARS 256
-
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-       
-    // Creating a hash array and initialising all the values as 0's
-        int hash[NO_OF_CHARS] = { 0 };
-        int i;
-        
-        int n1 = s.length();
-        int n2 = t.length();
-        
-        // If length of both strings is not same, then they cannot be anagram.
-        if( n1 != n2) return false;
-        
-        /*
-       For each character in input strings, 
-       increment count in the corresponding count array.
-       and decrement the count in the anagram array
-       */
-        for( i = 0 ; i < n1 ; i++ ){
-            hash[s[i]]++;
-            hash[t[i]]--;
+        unordered_map<char,int>sm,tm;
+        for(auto it : s){
+            sm[it-97]++;
         }
-        
-        // check if all values are 0
-        for( i=0 ; i< n1 ; i++){
-            if(hash[s[i]] != 0){
-                return false;
-            }
+        for(auto it : t){
+            tm[it-97]++;
         }
-        return true;
+        return sm==tm;
     }
 };
