2e358a7d4b5cd576fae625ea0e622ae67d7ff2a4
Aman Ali (He/Him) <amanali.siddiqui2020@vitstudent.ac.in>
2022-07-31 10:14:56 +0530

    Time: 537 ms (100.00%), Space: 135.2 MB (100.00%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
@@ -3,42 +3,35 @@ public:
     int maxLength = -1;
 
     void getcycle(vector<int> &edges,int si,vector<bool>& visit,int size,vector<int>& store){
-        if(si == -1)return ;
+        if(si==-1) return;
         if(visit[si]){
-            int count = -1;
-            for(int i =0;i<store.size();i++){
+            int count =-1;
+            for(int i = 0 ; i<store.size();i++){
                 if(store[i]==si){
                     count = i;
                     break;
-                } }
-            
+                }
+            }
             if(count==-1)return;
-            maxLength = max(maxLength,size-count);  // size-count gives the proper size of cycle
-            return ;
+            maxLength=max(maxLength,size-count);
+            return;
         }
-        
-        visit[si] = true;
+        visit[si]=true;
         store.push_back(si);
         getcycle(edges,edges[si],visit,size+1,store);
-        
-        return ;
-        
+        return;
     }
     
     
     
-    int longestCycle(vector<int>& edges) {
-        
-        vector<bool> visit(edges.size(),0);
-        for(int i =0;i<edges.size();i++){
-            
-            if(visit[i])continue;
+    int longestCycle(vector<int>& e) {
+        int n = e.size();
+        vector<bool> visit(n,0);
+        for(int i =0;i<n;i++){
+            if(visit[i]) continue;
             vector<int> store;
-            getcycle(edges,i,visit,0,store);
-     
+            getcycle(e,i,visit,0,store);
         }
-        
         return maxLength;
-        
     }
 };
