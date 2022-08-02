dc8a24382db93a50aa98f30f3da52c34afabb451
Aman Ali (He/Him) <amanali.siddiqui2020@vitstudent.ac.in>
2022-08-02 13:45:47 +0530

    Time: 9 ms (54.46%), Space: 10.1 MB (59.63%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -14,7 +14,10 @@ public:
             int n = a.size(),L = initL , R =n-1;
             while(L<=R){
                 int M = L +(R-L)/2;
-                // M-1<initL ?
+                // 1 || 2
+                // 1 executed when we first time encounter a value such that M-1th indesx is less than M th index
+                // 2 is if we already encountered an element before now we are checking if the duplicate is present , i.e. in case of 2 it is present mulriple times, hene this logic accounts for multiple occurances of 2
+                // while using this approach we need to sort both the arrays to make this work, as we are moving ahead and restrincting our search space after every outer iterative step
                 if( (M-1<initL || a[M-1]<t) && a[M] == t ) {
 
                     ans.push_back(t);
