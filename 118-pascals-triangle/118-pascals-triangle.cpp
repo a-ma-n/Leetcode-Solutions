ada53238944acc6bf6362b26f15cd9a72aabe147
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2022-08-04 15:06:44 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (71.27%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
   vector<vector<int>> generate(int n) {      
-   vector<vector<int>> r(n); 
-      for(int i =0;i<n;i++){
-          r[i] = vector<int>(i+1,1);        
-          for(int j =1;j<i;j++) // i<n && j<i => so last element isskipped
-              r[i][j]=r[i-1][j]+r[i-1][j-1];
-    }
-  return r;
-  }    
+    vector<vector<int>> v(n);
+       for(int i = 0 ; i < n ;i++ ){
+           v[i]= vector<int>(i+1,1);
+           for(int j = 1 ; j < i; j++ )
+               v[i][j] = v[i-1][j]+v[i-1][j-1];
+       }
+      return v;
+  }
 };
