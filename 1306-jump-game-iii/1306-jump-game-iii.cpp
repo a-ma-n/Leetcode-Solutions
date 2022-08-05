2252b3a2e6c1f7833f2a38d63b8799a56fdc7eeb
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2022-08-05 09:54:03 +0530

    Time: 100 ms (19.04%), Space: 30.7 MB (99.62%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
+    // check if you can reach to any index with value 0.
+    // https://leetcode.com/problems/jump-game-iii/discuss/1619031/C%2B%2BPython-Simple-Solution-w-Explanation-or-DFS-and-BFS-Traversals
     bool canReach(vector<int>& arr, int cur) {
         if(cur<0 || cur>size(arr) || arr[cur]<0) return false;
         arr[cur]*=-1;
+        // returns true if arr[cur]==0
         return !arr[cur] || canReach(arr,cur+arr[cur]) || canReach(arr,cur-arr[cur]);
     }
 };
