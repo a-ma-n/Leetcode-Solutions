c06200ebf9dea9167edb02010353c423476b5f6c
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2022-08-05 09:53:12 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (65.95%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int combinationSum4(vector<int>& N, int T) {
-        vector<unsigned int>dp(T+1,0);
+        vector<unsigned int>dp(T+1,0); // stores the count of combinations making this sum
         dp[0]=1;
         for(int i=0;i<T;i++){
             if(!dp[i]) continue;
