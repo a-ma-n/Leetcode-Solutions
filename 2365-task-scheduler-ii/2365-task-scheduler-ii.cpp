bd4be8e70b8a43d2c07bbca83ed444f273be5182
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2022-08-06 23:14:50 +0530

    Time: 424 ms (100.00%), Space: 102.2 MB (100.00%) - LeetHub

---
+++ 2365-task-scheduler-ii/2365-task-scheduler-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    long long taskSchedulerII(vector<int>& tasks, int space) {
+        unordered_map<int,long long> last;
+        long long res=0 ; 
+        for(int a :tasks)
+            if(last.count(a))
+                last[a] = res  = max(res,last[a]+space) + 1;
+            else
+                last[a] = ++res;
+        return res;
+    }
+};
