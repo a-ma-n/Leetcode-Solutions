7753d57e4d5d0f0aa08f86bfefa0373d8a0da190
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2022-08-07 10:31:54 +0530

    Time: 156 ms (100.00%), Space: 93 MB (11.11%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     // we execute this soln in such a way , that after incrementing i we make it equal to the size
-  /*  bool solve(vector<int> &nums,int i,vector<int> dp){
+    bool solve(vector<int> &nums,int i,vector<int>& dp){
         if(i == nums.size()) return true;
         if(i > nums.size()) return false;
         if(dp[i] != -1) return dp[i];
@@ -19,8 +19,8 @@ public:
             if(solve(nums,i+3,dp)) return true;
         // if none of the cases are satisfied then return false
         return dp[i]=false;
-    }*/
-    bool solve(vector<int>& nums, int i, vector<int>& dp){
+    }
+    /*bool solve(vector<int>& nums, int i, vector<int>& dp){
         if(i == nums.size()) return true;
         if(i > nums.size()) return false;
         if(dp[i] != -1) return dp[i];
@@ -35,7 +35,7 @@ public:
             if(solve(nums, i + 3, dp)) return true;
         }
         return dp[i] = false;
-    }
+    }*/
     bool validPartition(vector<int>& nums) {
         // if size is 2 we check for this and return
         if(nums.size()==2) return nums[0]==nums[1];
