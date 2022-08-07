0b0e51baf23b2dfc90e4efb927cbb1c8f5713078
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2022-08-07 18:44:20 +0530

    Time: 9 ms (86.46%), Space: 6.9 MB (91.76%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,60 +1,17 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        vector<int> mpp(256,-1);
-        
-        int left = 0 , right= 0;
-        int n = s.size();
-        int len=0;
-        while(right<n){
-            if(mpp[s[right]] != -1) // if it does exist
-                left=max(mpp[s[right]]+1,left);
-            
-            mpp[s[right]] = right;
-            
-            len = max(len,right - left + 1);
-            right++;
+        // 98-122
+       int i = 0 , j = 0 , N = s.size(),ans=0,cnt[128]={};
+       for (; j < N; ++j) {
+            cnt[s[j]]++;
+             while (cnt[s[j]] > 1) cnt[s[i++]]--;
+           // window will be of size j-i+1
+            ans = max(ans, j - i + 1);
         }
-        return len;
+        return ans;
+        
+        
+        
     }
 };
-//    vector < int > mpp(256, -1);
-
-//       int left = 0, right = 0;
-//       int n = s.size();
-//       int len = 0;
-//       while (right < n) {
-//         if (mpp[s[right]] != -1)
-//           left = max(mpp[s[right]] + 1, left);
-
-//         mpp[s[right]] = right;
-
-//         len = max(len, right - left + 1);
-//         right++;
-//       }
-//       return len;
-//     }
-
-
-//         map<int,int> mpp;
-        
-//         int l=0,r=0,maxLength=0,i,length=0;
-        
-//         for(;r<s.length();r++){
-            
-//             if(mpp.find(s[r])==mpp.end()){//element not present
-//                 mpp[s[r]]=i;
-//                 cout<<"r:"<<r<<endl;
-//                 length++;
-//                 maxLength=max(maxLength,length);
-//             }
-//             else{//element present
-                
-//                 l=mpp[s[r]]+1;
-//                 length=0;
-//                 mpp[s[r]]=l;
-//                 cout<<"l:"<<l<<endl;
-//             }
-            
-//         }
-//         return maxLength;
