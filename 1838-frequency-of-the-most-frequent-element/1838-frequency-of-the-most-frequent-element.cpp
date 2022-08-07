db5d7a5d243af317cc8583f71aa4661a4c8a4e2d
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2022-08-07 17:17:56 +0530

    Time: 381 ms (47.39%), Space: 99 MB (89.37%) - LeetHub

---
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxFrequency(vector<int>& A, int k) {
+        
+        if(A.size()==1) return 1;
+        sort(A.begin(),A.end());
+        long i = 0,j=0,sum=0, N = A.size();
+        for(; j < N ; ++j){
+             sum += A[j];
+            // We know that if we increase the value of the numbers they will become the same number after sorting and hence we do j-i+1 * A[j] , difference of this and the sum (of the numbers we found which are in our sliding window)
+            // if difference of these two is <=  k => we can consider them in our max freq range => dont increase i(size of window) &expand window size by increasing j
+            // if the difference is greater than k=> we need to delete the first elements of the window and hence we will be abel to get a window where the diff<=k 
+            if((j-i+1)*A[j] - sum > k) sum-=A[i++];
+        }
+        // we are going to return the size of the window as it is the number of elements which can be taken
+        // we did j-i & not j-i+1 as j is already>n
+        return j-i;
+    }
+};
