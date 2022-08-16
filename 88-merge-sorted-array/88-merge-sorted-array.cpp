c5d9248a1d3ea2b0c46bc7b3b5838542125f84ad
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2022-08-16 11:18:27 +0530

    Time: 7 ms (28.12%), Space: 9 MB (89.77%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-        void merge(vector<int>& numbers1, int size1, vector<int>& numbers2, int size2)
+      void merge(vector<int>& numbers1, int size1, vector<int>& numbers2, int size2)
     {                        
         int index1 = size1 - 1, index2 = size2 - 1, validIndex = numbers1.size() - 1;
         while (index1 >= 0 && index2 >= 0)
