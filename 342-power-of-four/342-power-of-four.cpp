024243c6c25ef9384d2f795a0eae255679ee15a5
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2022-08-22 10:24:58 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (24.97%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -3,8 +3,7 @@ public:
     /*
     https://leetcode.com/problems/power-of-four/discuss/482197/C%2B%2B-1-line-code-faster-than-100-(0-ms)-with-explanation-no-loopsrecursion
     */
-    bool isPowerOfFour(int &n) {
-        //return((n&1)==1 && ((n>>1)&1)==1) ;
+    bool isPowerOfFour(const int &n) {
         return n>0 && (n&(n-1))==0 && (n&0xAAAAAAAA)==0;
     }
 };
