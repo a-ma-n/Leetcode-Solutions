83951e03682f8a1e375c532f68001f6b927afb73
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2022-09-09 23:36:23 +0530

    Time: 70 ms (94.56%), Space: 14.7 MB (21.07%) - LeetHub

---
+++ 622-design-circular-queue/622-design-circular-queue.py
@@ -0,0 +1,76 @@
+from threading import Lock
+
+class MyCircularQueue:
+
+    def __init__(self, k: int):
+        """
+        Initialize your data structure here. Set the size of the queue to be k.
+        """
+        self.queue = [0]*k
+        self.headIndex = 0
+        self.count = 0
+        self.capacity = k
+        # additional atttribute to protect the access of our queue
+        self.queueLock = Lock()
+        
+    def enQueue(self, value: int) -> bool:
+        """
+        Insert an element into the circular queue. Return true if the operation is successful.
+        """
+        with self.queueLock:
+            if self.count == self.capacity:
+                return False
+            self.queue[(self.headIndex + self.count) % self.capacity] = value
+            self.count += 1
+        # automatically release the lock when leaving the block
+        return True
+
+    def deQueue(self) -> bool:
+        """
+        Delete an element from the circular queue. Return true if the operation is successful.
+        """
+        if self.count == 0:
+            return False
+        self.headIndex = (self.headIndex + 1) % self.capacity
+        self.count -= 1
+        return True
+        
+    def Front(self) -> int:
+        """
+        Get the front item from the queue.
+        """
+        if self.count == 0:
+            return -1
+        return self.queue[self.headIndex]
+
+    def Rear(self) -> int:
+        """
+        Get the last item from the queue.
+        """
+        # empty queue
+        if self.count == 0 :
+            return -1
+        return self.queue[(self.headIndex + self.count - 1) % self.capacity]
+    
+    def isEmpty(self) -> bool:
+        """
+        Checks whether the circular queue is empty or not.
+        """
+        return self.count == 0
+        
+    def isFull(self) -> bool:
+        """
+        Checks whether the circular queue is full or not.
+        """
+        return self.count == self.capacity
+        
+
+
+# Your MyCircularQueue object will be instantiated and called as such:
+# obj = MyCircularQueue(k)
+# param_1 = obj.enQueue(value)
+# param_2 = obj.deQueue()
+# param_3 = obj.Front()
+# param_4 = obj.Rear()
+# param_5 = obj.isEmpty()
+# param_6 = obj.isFull()
