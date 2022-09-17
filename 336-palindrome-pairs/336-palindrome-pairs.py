d59fe84b8f63641cfbd9b635a813094b24a70665
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2022-09-17 12:00:47 +0530

    Time: 4833 ms (53.23%), Space: 400.4 MB (10.63%) - LeetHub

---
+++ 336-palindrome-pairs/336-palindrome-pairs.py
@@ -0,0 +1,42 @@
+class TrieNode:
+    def __init__(self):
+        self.next = collections.defaultdict(TrieNode)
+        self.ending_word = -1
+        self.palindrome_suffixes = []
+
+class Solution:
+    def palindromePairs(self, words):
+
+        # Create the Trie and add the reverses of all the words.
+        trie = TrieNode()
+        for i, word in enumerate(words):
+            word = word[::-1] # We want to insert the reverse.
+            current_level = trie
+            for j, c in enumerate(word):
+                # Check if remainder of word is a palindrome.
+                if word[j:] == word[j:][::-1]:# Is the word the same as its reverse?
+                    current_level.palindrome_suffixes.append(i)
+                # Move down the trie.
+                current_level = current_level.next[c]
+            current_level.ending_word = i
+
+        # Look up each word in the Trie and find palindrome pairs.
+        solutions = []
+        for i, word in enumerate(words):
+            current_level = trie
+            for j, c in enumerate(word):
+                # Check for case 3.
+                if current_level.ending_word != -1:
+                    if word[j:] == word[j:][::-1]: # Is the word the same as its reverse?
+                        solutions.append([i, current_level.ending_word])
+                if c not in current_level.next:
+                    break
+                current_level = current_level.next[c]
+            else: # Case 1 and 2 only come up if whole word was iterated.
+                # Check for case 1.
+                if current_level.ending_word != -1 and current_level.ending_word != i:
+                    solutions.append([i, current_level.ending_word])
+                # Check for case 2.
+                for j in current_level.palindrome_suffixes:
+                    solutions.append([i, j])
+        return solutions
