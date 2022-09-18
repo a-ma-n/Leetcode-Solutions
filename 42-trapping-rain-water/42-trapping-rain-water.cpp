b05bcfbd9135ef5330ec6984e8ea406dd8612bf2
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2022-09-18 14:32:16 +0530

    Time: 45 ms (7.98%), Space: 19.6 MB (96.61%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int left =0, right = height.size()-1;
+        int left = 0, right = height.size()-1;
         int ans = 0;
         int left_max = 0, right_max = 0;
         while(left<right){ // till we reach the middle (or any point between the outer 2 limits)
-            if(height[left] <= height[right]){
+            if(height[left] < height[right]){
                 // while height of left is less than right height, store the max left height and store the trapped rainwater
                 height[left]>=left_max?(left_max = height[left])  : ans+=(left_max-height[left]);
                 // keep moving left index
@@ -13,7 +13,7 @@ public:
             }
             else{
                 //
-                height[right]>=right_max?(right_max = height[right]):ans += (right_max-height[right]);
+                height[right]>=right_max?(right_max = height[right]) : ans += (right_max-height[right]);
                 // move the right index backwards
                 --right;
             }
