bd45a5e304146ca4dd97a7ae9328f4d42e525d19
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2022-09-20 17:00:32 +0530

    Time: 2477 ms (89.88%), Space: 39.2 MB (74.09%) - LeetHub

---
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findLength(self, A: List[int], B: List[int]) -> int:
+        # +1 for safety in both the dimensions
+        memo = [[0]*(len(B)+1) for _ in range(len(A)+1)]
+        # we dont touch the extra uninit cells
+        for i in range(len(A) - 1, -1, -1):
+            for j in range(len(B) - 1, -1, -1):
+                if A[i] == B[j]:
+                    memo[i][j] = memo[i + 1][j + 1] + 1
+        return max(max(row) for row in memo)
