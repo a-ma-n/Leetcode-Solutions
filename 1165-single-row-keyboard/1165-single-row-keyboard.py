7cc12e2d03fd16d22160478442ca6fb586f806c7
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2022-12-03 15:38:10 +0530

    Time: 107 ms (36.22%), Space: 13.9 MB (84.81%) - LeetHub

--- 1165-single-row-keyboard/1165-single-row-keyboard.py
+++ 1165-single-row-keyboard/1165-single-row-keyboard.py
@@ -1,13 +1,10 @@
 class Solution:
     def calculateTime(self, keyboard: str, word: str) -> int:    
         wordMap={letter:index for index,letter in enumerate(keyboard)}
-        #print(wordMap)
         previousPosn=0
         sum=0
         for index,letter in enumerate(word):
-            diff=wordMap[letter]-previousPosn
-            sum+=abs(diff)
-            # print(diff)
+            sum+=abs(wordMap[letter]-previousPosn)
             previousPosn=wordMap[letter]
         return sum
         
