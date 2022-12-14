bb52ed0da06a42ebc65f07c9f9794a2b8ad7e9e1
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2022-12-14 13:59:41 +0530

    Time: 29 ms (97.15%), Space: 13.8 MB (66.57%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,15 +1,7 @@
 class Solution:
-    maxAmount=0
-    
     def rob(self, nums: List[int]) -> int:
-        @cache
-        def findAmount(index,profit):
-            self.maxAmount=max(self.maxAmount,profit)
-            if index<0: return 
-            # print(index,profit,nums[index])
-            findAmount(index-2,profit+nums[index])
-            findAmount(index-1,profit)
-        
-        findAmount(len(nums)-1,0)
-        
-        return self.maxAmount
+        second_prev=prev=0
+        for n in nums:
+            second_prev,prev=prev,max(n+second_prev,prev)
+        return prev
+            
