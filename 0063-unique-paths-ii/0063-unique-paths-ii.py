749516ec32c1e864d712a2b313020feca85be020
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2022-12-25 17:53:57 +0530

    Time: 36 ms (98.84%), Space: 13.9 MB (75.34%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,14 +1,28 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        row,col=len(obstacleGrid),len(obstacleGrid[0])
-        
-        @cache
-        def getNumberOfPaths(i,j):
-            # print(i,j)
-            if  i>=row or j>=col or obstacleGrid[i][j]==1 :
-                return 0
-            if i==row-1 and j==col-1: return 1
+        m = len(obstacleGrid)
+        n = len(obstacleGrid[0])
+        # no paths to the destination.
+        if obstacleGrid[0][0] == 1:
+            return 0
 
-            return getNumberOfPaths(i,j+1)+getNumberOfPaths(i+1,j)
-        
-        return getNumberOfPaths(0,0)
+        # Number of ways of reaching the starting cell = 1.
+        obstacleGrid[0][0] = 1
+
+        # Filling the values for the first column
+        for i in range(1,m):
+            obstacleGrid[i][0] = int(obstacleGrid[i][0] == 0 and obstacleGrid[i-1][0] == 1)
+
+        # Filling the values for the first row        
+        for j in range(1, n):
+            obstacleGrid[0][j] = int(obstacleGrid[0][j] == 0 and obstacleGrid[0][j-1] == 1)
+            
+        for i in range(1,m):
+                for j in range(1,n):
+                    
+                    if obstacleGrid[i][j]==0:
+                        obstacleGrid[i][j]=obstacleGrid[i-1][j]+obstacleGrid[i][j-1]
+                    else:
+                        obstacleGrid[i][j]=0
+                        
+        return obstacleGrid[m-1][n-1]
