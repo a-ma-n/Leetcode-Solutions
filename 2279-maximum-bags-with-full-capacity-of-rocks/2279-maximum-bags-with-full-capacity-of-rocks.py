d26759c6190cd047d3547ebbff0601dbdca679e1
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2022-12-27 10:36:37 +0530

    Time: 962 ms (88.16%), Space: 22.1 MB (87.83%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.py
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.py
@@ -4,15 +4,9 @@ class Solution:
         for rock,cap in zip(rocks,capacity): differences.append(cap-rock)
         differences.sort()
         countOfFilledBags=0
-        # print(differences)
-        for diff in differences:
-            if additionalRocks<diff: break
+        for index,diff in enumerate(differences):
+            if additionalRocks<diff: return index
             elif diff!=0: 
                 additionalRocks-=diff
             countOfFilledBags+=1
-                
-            # else: 
-            #     countOfFilledBags+=1
-                
-            # print(countOfFilledBags,diff)
-        return countOfFilledBags
+        return index+1
