72a7f54e117c045d0e697d4f9b996b441aee720b
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2022-12-27 10:39:26 +0530

    Time: 935 ms (94.41%), Space: 22.3 MB (49.34%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.py
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.py
@@ -1,7 +1,7 @@
 class Solution:
     def maximumBags(self, capacity: List[int], rocks: List[int], additionalRocks: int) -> int:
-        differences=sorted([ (cap-rock) for rock,cap in zip(rocks,capacity)])
+        differences=sorted([(cap-rock) for rock,cap in zip(rocks,capacity)])
         for index,diff in enumerate(differences):
             if additionalRocks<diff: return index
-            if diff!=0: additionalRocks-=diff
+            additionalRocks-=diff
         return index+1
