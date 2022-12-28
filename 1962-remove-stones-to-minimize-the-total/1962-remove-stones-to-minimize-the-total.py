84fec8bb22d10b1590add145c227664a0325f8e4
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2022-12-28 15:58:28 +0530

    Time: 1762 ms (95.88%), Space: 28.9 MB (18.87%) - LeetHub

--- 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.py
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.py
@@ -4,9 +4,7 @@ class Solution:
         sortedPiles=[-1*i for i in piles]
         heapq.heapify(sortedPiles)
         while (k:=k-1)+1>0:
-            print(k)
             heappush(sortedPiles, heappop(sortedPiles)//2)
-        print(sortedPiles)
         return -1*sum(sortedPiles)
             
         
