bbfe573c7232aca62ad03beebb4a7bfb299f573c
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2023-01-01 10:53:08 +0530

    Time: 32 ms (85.55%), Space: 13.8 MB (99.08%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -1,16 +1,15 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         wordMap=dict()
-        wordList=s.split()
-        if len(pattern)!=len(wordList): return False
-        for index,patternLetter in enumerate(pattern):
-            
-            if patternLetter not in wordMap:
-                if wordList[index] in wordMap.values(): 
-                    return False
-                wordMap[patternLetter]=wordList[index]
-            elif wordMap[patternLetter] != wordList[index]: 
-                return False
-            
+        if len(pattern)!=len(wordList:=s.split()): return False
+        for patternLetter,word in zip(pattern,wordList):
+            if patternLetter not in wordMap and word not in wordMap.values(): wordMap[patternLetter]=word
+            elif patternLetter in wordMap and wordMap[patternLetter]!=word: return False
+            elif patternLetter not in wordMap and word in wordMap.values(): return False
+                
+#         for index,patternLetter in enumerate(pattern):
+#             if patternLetter not in wordMap:
+#                 if wordList[index] in wordMap.values(): return False
+                
+#             elif wordMap[patternLetter] != wordList[index]:  return False
         return True
-        
