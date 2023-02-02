28a80c1dfffd002c272071d404fc68c200dc88d4
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2023-02-02 12:34:16 +0530

    Time: 1959 ms (38.81%), Space: 16.1 MB (28.20%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,20 +1,28 @@
 from functools import cache
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        coins.sort()
-        @cache
-        def traverse(ind, tar):
-            if ind==0:
-                if tar%coins[0]==0:
-                    return tar// coins[0]
-                return 1e9
-            notPick=traverse(ind-1,tar)
-            pick=1e9
-            if coins[ind]<=tar:
-                pick=1+traverse(ind,tar-coins[ind])
-            return min(pick,notPick)
-        ans=traverse(len(coins)-1,amount)
-        if ans==1e9: 
+        # coins.sort()
+        n=len(coins)
+        dp=[[0]*(amount+1) for i in range(n)]
+        #base cases
+        for i in range(amount+1):
+            if i%coins[0]==0:
+                dp[0][i]=i//coins[0]
+            else:
+                dp[0][i]=1e9
+                
+        for ind in range(1,n):
+            for target in range(amount+1):
+                notPick=dp[ind-1][target]
+                pick=1e9
+                if(coins[ind]<=target):
+                    pick=1+dp[ind][target-coins[ind]]
+                dp[ind][target]=min(pick,notPick)
+                
+        ans=dp[n-1][amount]
+        if ans>=1e9:
             return -1
-        else: return ans
-            
+        else: 
+            return ans
+        
+      
