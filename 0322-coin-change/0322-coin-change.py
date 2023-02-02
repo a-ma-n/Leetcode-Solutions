d6eb6740293b82fe60e528fe14873ec91832e01a
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2023-02-02 12:38:55 +0530

    Time: 1608 ms (56.94%), Space: 14.4 MB (38.29%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -3,23 +3,26 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         # coins.sort()
         n=len(coins)
-        dp=[[0]*(amount+1) for i in range(n)]
+        prev=[0]*(amount+1)
+        cur=[0]*(amount+1)
+        # dp=[[0]*(amount+1) for i in range(n)]
         #base cases
         for i in range(amount+1):
             if i%coins[0]==0:
-                dp[0][i]=i//coins[0]
+                prev[i]=i//coins[0]
             else:
-                dp[0][i]=1e9
+                prev[i]=1e9
                 
         for ind in range(1,n):
             for target in range(amount+1):
-                notPick=dp[ind-1][target]
+                notPick=prev[target]
                 pick=1e9
                 if(coins[ind]<=target):
-                    pick=1+dp[ind][target-coins[ind]]
-                dp[ind][target]=min(pick,notPick)
+                    pick=1+cur[target-coins[ind]]
+                cur[target]=min(pick,notPick)
+            prev=cur
                 
-        ans=dp[n-1][amount]
+        ans=prev[amount]
         if ans>=1e9:
             return -1
         else: 
