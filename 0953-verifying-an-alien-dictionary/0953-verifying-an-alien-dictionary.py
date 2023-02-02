281f52037f6b9b7c8d7dd556b59acef855601f68
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2023-02-02 10:12:01 +0530

    Time: 31 ms (95.75%), Space: 13.9 MB (77.66%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -3,20 +3,14 @@ class Solution:
         
         def areLexicographicallySorted(word1,word2):
             minLen=min(len(word1),len(word2))
+            if word1[:minLen]==word2[:minLen]: return len(word1)<=len(word2)
             for i in range(minLen):
-                # print(order.index(word1[i]),order.index(word2[i]),word1[i],word2[i])
                 if order.index(word1[i])<order.index(word2[i]):
                     return True
                 elif order.index(word1[i])>order.index(word2[i]):
                     return False
-                else:
-                    continue
-            print(word1[:minLen],word2[:minLen],len(word1),len(word2),len(word1)<len(word2))
-            if word1[:minLen]==word2[:minLen]: return len(word1)<=len(word2)
             return True
     
         for i in range(1,len(words)):
-            if not areLexicographicallySorted(words[i-1],words[i]):
-                print(words[i-1],words[i])
-                return False
+            if not areLexicographicallySorted(words[i-1],words[i]): return False
         return True
