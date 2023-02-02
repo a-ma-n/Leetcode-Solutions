a1f96c7751dc29587c58eeb225019c4890172b72
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2023-02-02 21:49:08 +0530

    Time: 163 ms (90.03%), Space: 19.3 MB (51.16%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,28 +1,21 @@
+# https://takeuforward.org/data-structure/target-sum-dp-21/
 from functools import cache
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        total=sum(nums)
-        dp=[[0]*(2*total+1) for i in range(len(nums))]
-        # base cases
-        dp[0][nums[0]+total]=1
-        dp[0][-nums[0]+total]+=1
-        
-        for i in range(1,len(nums)):
-            for sumVal in range(-total,total+1):
-                if dp[i-1][sumVal+total]>0:
-                    dp[i][sumVal+nums[i]+total]+=dp[i-1][sumVal+total]
-                    dp[i][sumVal-nums[i]+total]+=dp[i-1][sumVal+total]
-        if abs(target)>total:
-            return 0
-        else:
-            return dp[len(nums)-1][target+total]
-        # @cache
-        # def findWays(ind,target):
-        #     if ind<0:
-        #         if target==0:
-        #             return 1
-        #         return 0
-        #     subtract=findWays(ind-1,target+nums[ind])
-        #     add=findWays(ind-1,target-nums[ind])
-        #     return add+subtract
-        # return findWays(len(nums)-1,target)
+        totSum=sum(nums)
+        if (totSum-target)%2==1: return 0
+        if totSum<target: return 0
+        s2=(totSum-target)/2
+        @cache
+        def findSum(ind,target):
+            if ind==0: 
+                if target==0 and nums[0]==0: return 2
+                if target==0: return 1
+                return int(nums[0]==target)
+            notPick=findSum(ind-1,target)
+            pick=0
+            if nums[ind]<=target:
+                pick=findSum(ind-1,target-nums[ind])
+            return pick+notPick
+        return findSum(len(nums)-1,s2)
+                
