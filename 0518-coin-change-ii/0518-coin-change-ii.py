c70f8ef18c22c71199c5e4d683e8302cf6a66d5d
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2023-02-03 18:05:54 +0530

    Time: 115 ms (99.47%), Space: 35.6 MB (26.80%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,14 +1,26 @@
 from functools import cache
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
+#         dp=[[0]*(amount+1) for i in range(len(coins)+1)]
+#         # target=0
+#         for i in range(len(coins)): dp[i][0]=1
+#         # shifting indexes by 1 
+#         # vat index -1 set val as 0
+#         for i in range(amount+1): dp[0][i]=0
+        
+#         for ind in range(1,len(coins)):
+#             for target in range(amount+1):
+#                 notPick=dp[ind-1][target]
+#                 pick=0
+#                 if coins[ind]<=target:
+#                     pick=dp[ind][target-coins[ind]]
+#                 dp[ind][target]=pick+notPick
+#         return dp[len(coins)][amount]
+        
         @cache
         def getCoins(ind,target):
-            if target==0: return 1
-            if ind<0:    
-                # print(target,)
-                # if target%coins[0]==0: return target//coins[0]
-                # # elif target==coins[0] : return 1
-                return 0
+            if ind==0:
+                return int(target%coins[0]==0)
             notPick=getCoins(ind-1,target)
             pick=0
             if coins[ind]<=target:
