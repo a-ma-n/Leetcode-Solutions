af26034ec8f207a837c6222df7bc044b6f6143d7
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2023-02-03 20:40:30 +0530

    Time: 530 ms (57.00%), Space: 13.9 MB (96.88%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -3,21 +3,16 @@ class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         l1,l2=len(text1),len(text2)
         # shifting of indexes -1 is at 0
-        dp=[[0]*(l2+1) for i in range(l1+1)]
-        for i in range(l1): dp[i][0]=0
-        for i in range(l2): dp[0][i]=0
+        k=max(l1,l2)
+        prev=[0]*(l2+1)
+        cur=[0]*(l2+1)
         for ind1 in range(1,l1+1):
             for ind2 in range(1,l2+1):
                 if text1[ind1-1]==text2[ind2-1]:
-                    dp[ind1][ind2]=1+dp[ind1-1][ind2-1]
+                    cur[ind2]=1+prev[ind2-1]
                 else:
-                    dp[ind1][ind2]=max(dp[ind1][ind2-1],dp[ind1-1][ind2])
-        return dp[l1][l2]
+                    cur[ind2]=max(cur[ind2-1],prev [ind2])
+            prev = cur.copy()
+        
+        return prev[l2]
     
-        # @cache
-        # def lcs(ind1,ind2):
-        #     if ind1<0 or ind2<0: return 0
-        #     if text1[ind1]==text2[ind2]:
-        #         return 1+lcs(ind1-1,ind2-1)
-        #     return max(lcs(ind1-1,ind2),lcs(ind1,ind2-1))
-        # return lcs(len(text1)-1,len(text2)-1)
