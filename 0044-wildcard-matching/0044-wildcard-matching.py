e505e54a3d3041d354fbd4735e9bce98926b9713
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2023-02-04 17:49:34 +0530

    Time: 632 ms (69.42%), Space: 13.9 MB (88.00%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.py
+++ 0044-wildcard-matching/0044-wildcard-matching.py
@@ -7,20 +7,23 @@ class Solution:
                 if r[i-1]!='*':
                     return False
             return True
-        
-        dp=[[False]*(len(s2)+1) for i in range(len(s1)+1)]
-        dp[0][0]=True
-        for i in range(1,len(s2)+1): dp[0][i]=False
-        for i in range(1,len(s1)+1): dp[i][0]=isAllStars(s1,i)
+        prev=[False]*(len(s2)+1)
+        cur=[False]*(len(s2)+1)
+        # dp=[[False]*(len(s2)+1) for i in range(len(s1)+1)]
+        prev[0]=True
+        # for i in range(1,len(s2)+1): dp[0][i]=False
+        # for i in range(1,len(s1)+1): dp[i][0]=isAllStars(s1,i)
         
         for ind1 in range(1,len(s1)+1):
+            cur[0]=isAllStars(s1,ind1)
             for ind2 in range(1,len(s2)+1):
                 if s1[ind1-1]==s2[ind2-1] or s1[ind1-1]=='?':
-                    dp[ind1][ind2]=dp[ind1-1][ind2-1]
+                    cur[ind2]=prev[ind2-1]
                 else:
                     if s1[ind1-1]=='*':
-                        dp[ind1][ind2]= dp[ind1-1][ind2] or dp[ind1][ind2-1]
+                        cur[ind2]= prev[ind2] or cur[ind2-1]
                     else:
-                        dp[ind1][ind2]= False
-        return dp[len(s1)][len(s2)]
+                        cur[ind2]= False
+            prev=cur.copy()
+        return prev[len(s2)]
          
