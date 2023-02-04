988dfc3711bc043d915d79efffc4ba49dbed47fd
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2023-02-04 13:26:21 +0530

    Time: 143 ms (81.73%), Space: 13.8 MB (99.91%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -2,26 +2,24 @@ from functools import cache
 class Solution:
     def minDistance(self, s1: str, s2: str) -> int:
         l1,l2=len(s1),len(s2)
-        dp=[[0]*(l2+1) for i in range(l1+1)]
-        for i in range(l1+1): 
-            dp[i][0]=i
-        for i in range(l2+1): 
-            dp[0][i]=i
+        # if l2>l1:
+        #     s1,s2=s2,s1
+        #     l1,l2=l2,l1
+            
+        prev=[0]*(l2+1)
+        cur=[0]*(l2+1)
+        for j in range(l2+1): 
+            prev[j]=j
+        
         for ind1 in range(1,l1+1):
+            cur[0]=ind1
             for ind2 in range(1,l2+1):
                 if s1[ind1-1]==s2[ind2-1]:
-                    dp[ind1][ind2]= dp[ind1-1][ind2-1]
+                    cur[ind2]= prev[ind2-1]
                 else:
-                    dp[ind1][ind2]= 1+min(dp[ind1-1][ind2-1],dp[ind1][ind2-1],dp[ind1-1][ind2])
+                    cur[ind2]= 1+min(prev[ind2-1],cur[ind2-1],prev[ind2])
+            prev=cur.copy()
             
-        return  dp[l1][l2]
-        # @cache
-        # def editDistanceUtil(ind1,ind2):
-        #     if ind1<0: return ind2+1
-        #     if ind2<0: return ind1+1
-        #     if s1[ind1]==s2[ind2]:
-        #         return editDistanceUtil(ind1-1,ind2-1)
-        #     return 1+min(editDistanceUtil(ind1-1,ind2-1),editDistanceUtil(ind1,ind2-1),editDistanceUtil(ind1-1,ind2))
-        # return  editDistanceUtil(len(s1)-1,len(s2)-1)
-            
+        return  prev[l2]
+      
             
