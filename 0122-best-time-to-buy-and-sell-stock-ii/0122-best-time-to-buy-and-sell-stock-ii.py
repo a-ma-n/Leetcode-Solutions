4a2246b317266aad20406b009c53803e956871c4
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2023-02-04 19:06:44 +0530

    Time: 103 ms (26.05%), Space: 17.4 MB (5.35%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,18 +1,19 @@
-from functools import cache
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n=len(prices)
-        @cache
-        def transact(ind,buy):
-            if ind==n: return 0
-            if buy:
-                buy=-prices[ind]+transact(ind+1,0)
-                notBuy=transact(ind+1,1)
-                profit=max(buy,notBuy)
-            else:
-                sell=prices[ind]+transact(ind+1,1)
-                dontSell=transact(ind+1,0)
-                profit=max(sell,dontSell)
-            return profit
-        return transact(0,1)
-                
+        dp=[[-1]*2 for i in range(n+1)]
+        dp[n][0]=dp[n][1]=0
+        
+        for ind in range(n-1,-1,-1):
+            for b in [0,1]:
+                if b:
+                    buy=-prices[ind]+dp[ind+1][0]
+                    notBuy=dp[ind+1][1]
+                    profit=max(buy,notBuy)
+                else:
+                    sell=prices[ind]+dp[ind+1][1]
+                    dontSell=dp[ind+1][0]
+                    profit=max(sell,dontSell)
+                dp[ind][b]=profit
+                print(ind,end=' ')
+        return dp[0][1]
