dfeb70347a94e16f0227995297759aa2fe869aa8
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2023-02-04 19:08:38 +0530

    Time: 88 ms (30.67%), Space: 17.1 MB (6.52%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -6,14 +6,13 @@ class Solution:
         
         for ind in range(n-1,-1,-1):
             for b in [0,1]:
-                if b:
-                    buy=-prices[ind]+dp[ind+1][0]
-                    notBuy=dp[ind+1][1]
+                if b==0:
+                    buy=-prices[ind]+dp[ind+1][1]
+                    notBuy=dp[ind+1][0]
                     profit=max(buy,notBuy)
                 else:
-                    sell=prices[ind]+dp[ind+1][1]
-                    dontSell=dp[ind+1][0]
+                    sell=prices[ind]+dp[ind+1][0]
+                    dontSell=dp[ind+1][1]
                     profit=max(sell,dontSell)
-                dp[ind][b]=profit
-                print(ind,end=' ')
-        return dp[0][1]
+                dp[ind][b]=profit        
+        return dp[0][0]
