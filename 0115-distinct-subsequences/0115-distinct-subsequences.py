5fc033ef27b41a083e401733d800c3bc59dad63a
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2023-02-04 12:49:52 +0530

    Time: 432 ms (82.18%), Space: 14.1 MB (91.02%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -2,18 +2,24 @@ from functools import cache
 class Solution:
     def numDistinct(self, s1: str, s2: str) -> int:
         l1,l2=len(s1),len(s2)
-        @cache
-        def countUtil(ind1,ind2):
-            if ind2<0: return 1
-            if ind1<0: return 0
+        
+        prev=[0]*(l2+1)
+        cur=[0]*(l2+1)
+        # dp=[[0]*(l2+1) for i in range(l1+1)]
+        prev[0]=1
+        # for j in range(1,l2+1): dp[0][j]=0 # leave the 1 in the row itself, if say bith are -1 then take 1
             
-            if s1[ind1]==s2[ind2]:
-                leaveOne=countUtil(ind1-1,ind2-1)
-                stay=countUtil(ind1-1,ind2)
+        for ind1 in range(1,l1+1):
+            for ind2 in range(l2,0,-1):
                 
-                return leaveOne+stay
-                
-            return countUtil(ind1-1,ind2)
-        return countUtil(l1-1,l2-1)
-            
+                if s1[ind1-1]==s2[ind2-1]:
+                    leaveOne=prev[ind2-1]
+                    stay=prev[ind2]
+                    prev[ind2]=leaveOne+stay
+                else:
+                    prev[ind2]=prev[ind2]
+            # prev=cur
+        return prev[l2]
+    
+       
             
