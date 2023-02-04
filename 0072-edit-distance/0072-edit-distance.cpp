96b2b364f1db68f6e4e78d93260379385e8c3ded
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2023-02-04 13:24:42 +0530

    Time: 12 ms (74.50%), Space: 6.5 MB (89.96%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minDistance(string S1, string S2) {
+     
+   int n = S1.size();
+    int m = S2.size();
+    
+    vector<int> prev(m+1,0);
+    vector<int> cur(m+1,0);
+    
+    for(int j=0;j<=m;j++){
+        prev[j] = j;
+    }
+    
+    for(int i=1;i<n+1;i++){
+        cur[0]=i;
+        for(int j=1;j<m+1;j++){
+            if(S1[i-1]==S2[j-1])
+                cur[j] = 0+prev[j-1];
+            
+            else cur[j] = 1+min(prev[j-1],min(prev[j],cur[j-1]));
+        }
+        prev = cur;
+    }
+    
+    return prev[m];
+    }
+};
