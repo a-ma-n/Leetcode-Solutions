a5c481254e618ac7cbff803ba508a8b467a020f9
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2023-02-05 12:46:43 +0530

    Time: 366 ms (34.14%), Space: 15.2 MB (27.00%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -0,0 +1,29 @@
+class Solution:
+    substrings=[]
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        # generate all substrings of s; sort them if they are equal then put index in ans
+        ns, np = len(s), len(p)
+        if ns < np:
+            return []
+        p_count = Counter(p)
+        s_count = Counter()
+        
+        output = []
+        # sliding window on the string s
+        for i in range(ns):
+            # add one more letter 
+            # on the right side of the window
+            s_count[s[i]] += 1
+            # remove one letter 
+            # from the left side of the window
+            if i >= np:
+                if s_count[s[i - np]] == 1:
+                    del s_count[s[i - np]]
+                else:
+                    s_count[s[i - np]] -= 1
+            # compare array in the sliding window
+            # with the reference array
+            if p_count == s_count:
+                output.append(i - np + 1)
+        
+        return output
