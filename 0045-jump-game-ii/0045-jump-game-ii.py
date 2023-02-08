376f21e0ca809798fdfed5c7bc13bac69969d09a
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2023-02-08 20:00:22 +0530

    Time: 7395 ms (19.74%), Space: 32.2 MB (5.11%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,18 +1,16 @@
-
+from functools import cache
+import sys
+sys.setrecursionlimit(1000000)
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        # The starting range of the first jump is [0, 0]
-        answer, n = 0, len(nums)
-        cur_end, cur_far = 0, 0
-        
-        for i in range(n - 1):
-            # Update the farthest reachable index of this jump.
-            cur_far = max(cur_far, i + nums[i])
+        n=len(nums)
 
-            # If we finish the starting range of this jump,
-            # Move on to the starting range of the next jump.
-            if i == cur_end:
-                answer += 1
-                cur_end = cur_far
-                
-        return answer
+        @cache
+        def traverse(ind):
+            if ind>=n-1:
+                return 0
+            minJumps=1e9
+            for i in range(1,nums[ind]+1):
+                minJumps=min(minJumps,1+traverse(ind+i))
+            return minJumps
+        return traverse(0)
