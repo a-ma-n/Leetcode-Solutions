83c6f0a100b9afb4351e65ce44413a7a80f4d039
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2023-02-10 00:10:52 +0530

    Time: 1093 ms (44.32%), Space: 23.5 MB (29.94%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,16 +1,19 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
         n=len(prices)
-        @cache
-        def transact(ind,buy):
-            if ind==n: return 0
-            if buy:
-                buy=-prices[ind]+transact(ind+1,0)
-                notBuy=transact(ind+1,1)
-                profit=max(buy,notBuy)
-            else:
-                sell=prices[ind]+transact(ind+1,1)-fee
-                dontSell=transact(ind+1,0)
-                profit=max(sell,dontSell)
-            return profit
-        return transact(0,1)
+        dp=[[-1]*2 for i in range(n+1)]
+        dp[n][0]=dp[n][1]=0
+        
+        for ind in range(n-1,-1,-1):
+            for b in [0,1]:
+                if b==0:
+                    buy=-prices[ind]+dp[ind+1][1]
+                    notBuy=dp[ind+1][0]
+                    profit=max(buy,notBuy)
+                else:
+                    sell=prices[ind]+dp[ind+1][0]-fee
+                    dontSell=dp[ind+1][1]
+                    profit=max(sell,dontSell)
+                dp[ind][b]=profit        
+        # we will get our result at the 0th index , i.e. we sold whatever stocks we had
+        return dp[0][0]
