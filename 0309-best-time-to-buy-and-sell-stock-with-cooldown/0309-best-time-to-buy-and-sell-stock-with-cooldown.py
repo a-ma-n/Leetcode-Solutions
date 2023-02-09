bf5cbfe00a7934e467ac535fc88f8ff47654472b
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2023-02-10 00:02:06 +0530

    Time: 45 ms (75.86%), Space: 14.4 MB (41.53%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -6,17 +6,19 @@ class Solution(object):
         """
         n=len(prices)
         
-        @cache
-        def transact(ind,buy):
-            if ind>=n: return 0
-            if buy:
-                buy=-prices[ind]+transact(ind+1,0)
-                notBuy=transact(ind+1,1)
-                profit=max(buy,notBuy)
-            else:
-                sell=prices[ind]+transact(ind+2,1)
-                dontSell=transact(ind+1,0)
-                profit=max(sell,dontSell)
-            return profit
+        dp=[[0]*2 for i in range(n+2)]
+        # dp[n][0]=dp[n][1]=0
         
-        return transact(0,1)
+        for ind in range(n-1,-1,-1):
+            for b in [0,1]:
+                if b==0:
+                    buy=-prices[ind]+dp[ind+1][1]
+                    notBuy=dp[ind+1][0]
+                    profit=max(buy,notBuy)
+                else:
+                    sell=prices[ind]+dp[ind+2][0]
+                    dontSell=dp[ind+1][1]
+                    profit=max(sell,dontSell)
+                dp[ind][b]=profit        
+        # we will get our result at the 0th index , i.e. we sold whatever stocks we had
+        return dp[0][0]
