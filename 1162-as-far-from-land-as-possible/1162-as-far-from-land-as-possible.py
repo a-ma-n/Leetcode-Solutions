6dfdc04b1e730fda59f0edf77cd84b5d5934e721
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2023-02-10 22:41:36 +0530

    Time: 587 ms (72.90%), Space: 15.5 MB (28.06%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.py
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.py
@@ -6,25 +6,16 @@ class Solution:
         for x in range(rows):
             for y in range(cols):
                 if grid[x][y]==1:
-                    queue.appendleft((x,y+1))
-                    queue.appendleft((x,y-1))
-                    queue.appendleft((x+1,y))
-                    queue.appendleft((x-1,y))
+                     queue.appendleft((x,y+1)),queue.appendleft((x,y-1)),queue.appendleft((x+1,y)),queue.appendleft((x-1,y))
         while queue:
-            size=len(queue)
             steps+=1
-            for k in range(size):
+            for k in range(len(queue)):
                 x,y=queue[-1]
                 queue.pop()
                 if x>=0 and y>=0 and x<rows and y<cols and grid[x][y]==0:
                     grid[x][y]=steps
-                    queue.appendleft((x,y+1))
-                    queue.appendleft((x,y-1))
-                    queue.appendleft((x+1,y))
-                    queue.appendleft((x-1,y))
-        # print("steps",steps)
-        if steps==1:
-            return -1 
+                    queue.appendleft((x,y+1)),queue.appendleft((x,y-1)),queue.appendleft((x+1,y)),queue.appendleft((x-1,y))
+        if steps==1: return -1 
         return steps-1
                                         
                     
