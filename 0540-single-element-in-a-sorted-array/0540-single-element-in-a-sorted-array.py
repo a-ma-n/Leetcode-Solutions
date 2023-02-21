329b9322e03e4314f1b777c2edb301d2e8fe6e73
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2023-02-21 09:41:55 +0530

    Time: 168 ms (92.86%), Space: 23.8 MB (39.30%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -1,21 +1,14 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
         low,high=0,len(nums)-1
-        n=len(nums)
         while low<=high:
             mid=low+(high-low)//2
-            if mid==0 or mid==n-1: break
-                
+            if mid==0 or mid==len(nums)-1: break
             if nums[mid]==nums[mid-1]:
-                if (mid-low+1)%2!=0:
-                    high=mid
-                else:
-                    low=mid+1
+                if (mid-low+1)%2!=0: high=mid
+                else: low=mid+1
             elif nums[mid]==nums[mid+1]:
-                if ((mid-1)-low-1)%2!=0:
-                    high=mid-1
-                else:
-                    low=mid
-            else:
-                break
+                if ((mid-1)-low-1)%2!=0: high=mid-1
+                else: low=mid
+            else: break
         return nums[mid]            
