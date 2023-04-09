f48ea3caacf5b1559d194705c751daf7f93b14ad
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2023-04-10 03:49:43 +0530

    Time: 845 ms (15.38%), Space: 46.6 MB (69.23%) - LeetHub

--- 2615-sum-of-distances/2615-sum-of-distances.py
+++ 2615-sum-of-distances/2615-sum-of-distances.py
@@ -7,14 +7,14 @@ class Solution:
         ans=[0]*len(nums)
         for key,val in mp.items():
             # for key,val in d1.items():
-            suff=sum(val)#suffix sum
-            pre=0#prefix sum
-            s=len(val)#len(suffix)
-            p=0#len(pre)
+            suffixSum=sum(val)#suffix sum
+            prefixSum=0#prefix sum
+            suffixLength=len(val)#len(suffix)
+            prefixLength=0#len(pre)
             for i in val:#iterate in the indexes value like [0,2,3]:
-                pre+=i #pass1:pre=0
-                p+=1#p=1
-                suff-=i#suff=5-0=5
-                s-=1#s=2
-                ans[i]=(-pre+p*i-s*i+suff)#ans[0]=(-0+1*0-2*0+5)=5
+                prefixSum+=i 
+                prefixLength+=1
+                suffixSum-=i
+                suffixLength-=1
+                ans[i]=(-prefixSum+prefixLength*i-suffixLength*i+suffixSum) #ans[0]=(-0+1*0-2*0+5)=5
         return ans
