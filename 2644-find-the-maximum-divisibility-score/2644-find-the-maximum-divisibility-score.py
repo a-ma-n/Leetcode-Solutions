c55fad77ef47ab76f401aae3216407216f7c2363
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2023-04-16 19:33:45 +0530

    Time: 4154 ms (100.00%), Space: 14.1 MB (100.00%) - LeetHub

---
+++ 2644-find-the-maximum-divisibility-score/2644-find-the-maximum-divisibility-score.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxDivScore(self, nums: List[int], divisors: List[int]) -> int:
+        ans=-1
+        most=-1
+        for d in divisors:
+            cnt=sum(1 for n in nums if n%d==0)
+            if cnt>most or cnt==most and d<ans:
+                ans=d
+                most=cnt
+
+        return ans        
