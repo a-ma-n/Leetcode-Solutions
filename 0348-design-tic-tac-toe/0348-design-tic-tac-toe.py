dcd6b17305039bc6628c18fbac2da26927910fb8
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2023-04-23 22:49:13 +0530

    Time: 82 ms (87.71%), Space: 16.7 MB (43.68%) - LeetHub

---
+++ 0348-design-tic-tac-toe/0348-design-tic-tac-toe.py
@@ -0,0 +1,22 @@
+class TicTacToe:
+
+    def __init__(self, n):
+        self.row, self.col, self.diag, self.anti_diag, self.n = [0] * n, [0] * n, 0, 0, n
+        
+    def move(self, row, col, player):
+        offset = player * 2 - 3
+        self.row[row] += offset
+        self.col[col] += offset
+        if row == col:
+            self.diag += offset
+        if row + col == self.n - 1:
+            self.anti_diag += offset
+        if self.n in [self.row[row], self.col[col], self.diag, self.anti_diag]:
+            return 2
+        if -self.n in [self.row[row], self.col[col], self.diag, self.anti_diag]:
+            return 1
+        return 0
+
+# Your TicTacToe object will be instantiated and called as such:
+# obj = TicTacToe(n)
+# param_1 = obj.move(row,col,player)
