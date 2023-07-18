5d743c4fc7820a468b0b169be63b947d0e27df86
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2023-07-18 15:41:51 +0530

    Time: 68 ms (85.18%), Space: 16.3 MB (85.81%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,20 @@
+class Solution:
+    def combinationSum(self, c: List[int], target: int) -> List[List[int]]:
+        N=len(c)
+        def recur(ind,t,combn):
+            if t<0: return
+            if ind==N: 
+                if t==0:
+                    self.combinations.append(combn.copy())
+                return 
+            # pick
+            # if c[ind]<target:
+            if c[ind]<=t:
+                combn.append(c[ind])
+                recur(ind,t-c[ind],combn)
+                combn.pop()
+            recur(ind+1,t,combn)
+        self.combinations=[]
+        recur(0,target,[])
+        return self.combinations
+            
