bc139acf23f1189af3e9d27440c8952714bbbb3d
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2023-07-26 21:56:23 +0530

    Time: 3295 ms (50.71%), Space: 30.5 MB (75.78%) - LeetHub

---
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minSpeedOnTime(self, dist: List[int], hour: float) -> int:
+        low=0
+        high=10**7
+        
+        # if
+        is_ontime = lambda s: sum(math.ceil(d / s) for d in dist[:-1]) + dist[-1] / s <= hour
+        
+        while low+1<high:
+            mid=low+(high-low)//2
+            if is_ontime(mid):
+                    high = mid
+            else:
+                    low = mid
+        return high if is_ontime(high) else -1
+        # return -1
+    
+    # import math
+    # class Solution:
+    #     def minSpeedOnTime(self, dist: List[int], hour: float) -> int:
+    #         is_ontime = lambda s: sum(math.ceil(d / s) for d in dist[:-1]) + dist[-1] / s <= hour
+    #         low, high = 0, 10**7
+    #         while low + 1 < high:
+    #             mid = low + (high - low) // 2
+    #             if is_ontime(mid):
+    #                 high = mid
+    #             else:
+    #                 low = mid
+    #         return high if is_ontime(high) else -1
