06bcffab6cd5095950fb0c12059e7af80b9a9b99
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2023-08-03 23:46:18 +0530

    Time: 76 ms (56.48%), Space: 16.8 MB (18.80%) - LeetHub

--- 0256-paint-house/0256-paint-house.py
+++ 0256-paint-house/0256-paint-house.py
@@ -5,30 +5,33 @@ class Solution:
         def recur(ind,lastVal):
             if ind<0:
                 return 0
-            
-            red=blue=green=1e9
-            
-            
-            # if lastVal=="temp":
             red=costs[ind][0]+recur(ind-1,"red")
             blue=costs[ind][1]+recur(ind-1,"blue")
             green=costs[ind][2]+recur(ind-1,"green")
-                # print(red,blue,green,costs[ind][0],costs[ind][1],costs[ind][2])
-                # return min(red,blue,green)
             
             if lastVal=="red":
-                # print(ind,"red",costs[ind][0])
-                # red=costs[ind][0]+recur(ind-1,"red")
                 return min(blue,green)
             elif lastVal=="blue":
-                # print(ind,"blue",costs[ind][1])
-                # blue=costs[ind][1]+recur(ind-1,"blue")
                  return min(red,green)
-            elif lastVal=="green": 
-                # print(ind,"green",costs[ind][2])
-                # green=costs[ind][2]+recur(ind-1,"green")
+            elif lastVal=="green":         
                 return min(blue,red)
             else:
                 return min(red,blue,green)
             
         return recur(len(costs)-1,"temp")
+    
+        
+        n=len(costs)
+        dp=[[1e9]*3 for _ in range(n+1)]
+        
+        dp[0][0]=dp[0][1]=dp[0][2]=0
+        dp[1][0]=costs[0][0]
+        dp[0][1]=costs[0][1]
+        dp[0][2]=costs[0][2]
+        
+        for i in range(2,n+1):
+            dp[i][0]+=min(dp[i-1][1],dp[i-1][2])
+            dp[i][1]+=min(dp[i-1][0],dp[i-1][2])
+            dp[i][2]+=min(dp[i-1][0],dp[i-1][1])
+        
+        return min(dp[n][0],dp[n][1],dp[n][2])
