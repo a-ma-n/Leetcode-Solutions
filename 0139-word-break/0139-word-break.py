293f176289ee8faffb1e8832d59d54875ae07691
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2023-08-04 13:04:40 +0530

    Time: 50 ms (73.20%), Space: 16.5 MB (60.84%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -3,18 +3,38 @@ class Solution:
         
         n=len(s)
         s+=" "
-        
         @cache
         def recur(ind):
-            # print(ind,s[:ind])
-            if ind>=n: return True
-            
+            if ind<0: return True
+
             isAnyTrue=False
-            for k in range(ind,n+1):
-                # print(ind,s[ind:k+1])
-                if s[ind:k+1] in wordDict:
-                    if recur(k+1):
+            for k in range(ind,-1,-1):
+                # print(s[k:ind+1])
+                if s[k:ind+1] in wordDict:
+                    if recur(k-1):
                         isAnyTrue=True
-                        
+
             return isAnyTrue
-        return recur(0)
+        return recur(n-1)
+            
+        dp=[0]*n
+        # @cache
+#         def recur(ind):
+#             if ind>=n: return True
+            
+#             isAnyTrue=False
+#             for k in range(ind,n+1):
+#                 if s[ind:k+1] in wordDict:
+#                     if recur(k+1):
+#                         isAnyTrue=True
+                        
+#             return isAnyTrue
+#         return recur(0)
+    
+            
+    
+    
+        
+        
+        
+        
