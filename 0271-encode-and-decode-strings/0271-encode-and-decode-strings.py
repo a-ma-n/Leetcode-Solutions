433762fa9534ea422ccd92f0857c6f354305cf75
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2023-08-11 11:15:48 +0530

    Time: 67 ms (94.51%), Space: 16.6 MB (82.05%) - LeetHub

---
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -0,0 +1,18 @@
+class Codec:
+    def encode(self, strs: List[str]) -> str:
+        """Encodes a list of strings to a single string.
+        """
+        return "Ω".join(strs)
+        
+        
+
+    def decode(self, s: str) -> List[str]:
+        """Decodes a single string to a list of strings.
+        """
+        return s.split("Ω")
+        
+
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(strs))
