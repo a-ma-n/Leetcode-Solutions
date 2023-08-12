e1568c1b39a2f126021d5104d4402463f03f6891
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2023-08-12 18:27:34 +0530

    Time: 37 ms (89.29%), Space: 16.4 MB (20.10%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -3,16 +3,29 @@ class Solution:
         
         
         # recursion:
-        @cache
-        def issafe(r,c):
-            return 0<=r<m and 0<=c<n
+#         @cache
+#         def issafe(r,c):
+#             return 0<=r<m and 0<=c<n
         
-        @cache
-        def recur(r,c):
-            if r==m-1 and c==n-1: return 1
-            if not issafe(r,c): return 0
+#         @cache
+#         def recur(r,c):
+#             if r==m-1 and c==n-1: return 1
+#             if not issafe(r,c): return 0
             
-            return recur(r+1,c)+recur(r,c+1)
+#             return recur(r+1,c)+recur(r,c+1)
         
-        return recur(0,0)
+#         return recur(0,0)
+        
+        # top down
+        
+        dp=[[1]*n for _ in range(m)]
+        
+        # for i in range(1,m):
+        #     dp[i][0]=1
+        # for j in range(1,n):
+        #     dp[0][j]=1
             
+        for i in range(1,m):
+            for j in range(1,n):
+                dp[i][j]=dp[i-1][j]+dp[i][j-1]
+        return dp[m-1][n-1]
