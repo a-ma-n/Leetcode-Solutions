e2f541104cc6cd1eacd52c5bcf9a51260e90c0f9
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2023-08-12 18:28:55 +0530

    Time: 43 ms (67.79%), Space: 16.4 MB (20.10%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -17,15 +17,18 @@ class Solution:
 #         return recur(0,0)
         
         # top down
+        # initialise with 1 as there will alwyas be a way due to no obstruction
         
-        dp=[[1]*n for _ in range(m)]
         
-        # for i in range(1,m):
-        #     dp[i][0]=1
-        # for j in range(1,n):
-        #     dp[0][j]=1
+        dp=[[0]*n for _ in range(m)]
+        
+        dp[0][0]=1
+        
+        for i in range(1,m): dp[i][0]=1
+        for j in range(1,n): dp[0][j]=1
             
         for i in range(1,m):
             for j in range(1,n):
                 dp[i][j]=dp[i-1][j]+dp[i][j-1]
+        # print(dp)
         return dp[m-1][n-1]
