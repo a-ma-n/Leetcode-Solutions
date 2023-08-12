f1663122dc259f57c10523791602babe3313d772
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2023-08-12 18:25:16 +0530

    Time: 39 ms (83.27%), Space: 17 MB (7.08%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,12 +1,18 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dp=[[1]*n for i in range(m)]
-        # for i in range(n):
-        #     dp[0][i]=1
-        # for i in range(m):
-        #     dp[i][0]=1
-        for i in range(1,m):
-            for j in range(1,n):
-                dp[i][j]=dp[i][j-1]+dp[i-1][j]
-        return dp[m-1][n-1]
         
+        
+        # recursion:
+        @cache
+        def issafe(r,c):
+            return 0<=r<m and 0<=c<n
+        
+        @cache
+        def recur(r,c):
+            if r==m-1 and c==n-1: return 1
+            if not issafe(r,c): return 0
+            
+            return recur(r+1,c)+recur(r,c+1)
+        
+        return recur(0,0)
+            
