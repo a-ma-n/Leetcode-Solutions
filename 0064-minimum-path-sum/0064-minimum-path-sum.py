10b507843954c2684955c334fde98ab0c4fe63b3
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2023-08-12 18:34:58 +0530

    Time: 114 ms (41.25%), Space: 22.5 MB (6.74%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,25 +1,19 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        # memoisation
-        # @cache
-        # def traverse(i,j):
-        #     if i==len(grid) or j==len(grid[0]):return float(inf)
-        #     if i==len(grid)-1 and j==len(grid[0])-1:return grid[i][j]
-        #     return grid[i][j]+min(traverse(i+1,j),traverse(i,j+1))
-        # return traverse(0,0)
         
-        rows,cols=len(grid),len(grid[0])
-        dp=[[0]*cols for _ in range(rows)]
-        print(dp,grid[0][0])
-        dp[0][0]=grid[0][0]
-        print(dp)
-        for i in range(1,rows):
-            dp[i][0]=grid[i][0]+dp[i-1][0]
-        for j in range(1,cols):
-            dp[0][j]=grid[0][j]+dp[0][j-1]
-        print(dp)
-        for i in range(1,rows):
-            for j in range(1,cols):
-                dp[i][j]=grid[i][j]+min(dp[i-1][j],dp[i][j-1])
-        print(dp)
-        return dp[rows-1][cols-1]
+        
+        # recursion
+        
+        m,n=len(grid),len(grid[0])
+        
+        def issafe(r,c): return (0<=r<m and 0<=c<n)
+        @cache
+        def minPath(r,c):
+            # print(r,c)
+            if r==m-1 and c==n-1:
+                return grid[m-1][n-1]
+            
+            if not issafe(r,c): return 1e9
+            
+            return grid[r][c]+min(minPath(r+1,c),minPath(r,c+1))
+        return minPath(0,0)
