29a626f6cd32d5cf66446469397bf770bac045a4
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2023-08-12 18:52:44 +0530

    Time: 64 ms (94.03%), Space: 19 MB (16.38%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,29 +1,31 @@
 class Solution:
     def minimumTotal(self, grid: List[List[int]]) -> int:
         
-        m=len(grid)
-        # recursion
-        @cache
-        def recur(r,c):
+#         m=len(grid)
+#         # recursion
+#         @cache
+#         def recur(r,c):
             
-            if not (0<=r<m and 0<=c<len(grid[r])): return 1e9
+#             if not (0<=r<m and 0<=c<len(grid[r])): return 1e9
             
-            if r==m-1:
-                return grid[r][c]
+#             if r==m-1:
+#                 return grid[r][c]
             
             
-            return grid[r][c]+min(recur(r+1,c),recur(r+1,c+1))
+#             return grid[r][c]+min(recur(r+1,c),recur(r+1,c+1))
         
-        return recur(0,0)
+#         return recur(0,0)
     
         # bottom up
         m=len(grid)
-        dp = [[0]*m for _ in range(m)]
+        dp = [[1e9]*m for _ in range(m)]
         dp[0][0]=grid[0][0]
+        
         for i in range(1,m):
             for j in range(len(grid[i])):
                 dp[i][j]=grid[i][j]+min(dp[i-1][j],dp[i-1][j-1])
-                
+        print(dp)
+        return min(dp[m-1])
         # for i in range()
     
     
