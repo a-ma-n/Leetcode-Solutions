d07c49f70743f1151b9f2ba37b8c83062c06e4c6
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2023-08-12 18:37:17 +0530

    Time: 112 ms (47.70%), Space: 22.5 MB (6.74%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -17,3 +17,15 @@ class Solution:
             
             return grid[r][c]+min(minPath(r+1,c),minPath(r,c+1))
         return minPath(0,0)
+    
+    
+        dp=[[1e9]*n for _ in range(m)]
+        dp[0][0]=grid[0][0]
+        
+        for i in range(m):dp[i][0]=dp[i-1][0]+grid[i][0]
+        for j in range(n):dp[0][j]=dp[0][j-1]+grid[0][j]
+        
+        for i in range(1,m):
+            for j in range(1,n):
+                dp[i][j]= grid [i][j] + min(dp[i-1][j],dp[i][j-1])
+        return dp[m-1][n-1]
