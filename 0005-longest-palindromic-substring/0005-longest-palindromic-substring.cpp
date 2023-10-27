b486ffdb6afbea1c2d324c8534355e4416eb37f2
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2023-10-27 22:19:01 +0530

    Time: 42 ms (61.30%), Space: 162.8 MB (23.42%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,55 @@
+/*
+    Time Complexity : O(N ^ 2)
+    Space Complexity: O(1)
+    Where N is the length of the string.
+*/
+
+#include <algorithm>
+class Solution {
+    // use lps in dp
+public:
+    int expandAroundCenter(string str, int left, int right) 
+{
+    int start = left, end = right;
+    int n = str.length();
+
+    // Expand the center.
+    while (start >= 0 && end < n && str[start] == str[end]) 
+    {
+        start--;
+        end++;
+    }
+    return end - start - 1;
+}
+    
+    string longestPalindrome(string str) {
+        int n = str.length();
+
+    if (n < 1) 
+    {
+        return "";
+    }
+
+    int start = 0, end = 0;
+
+    for (int i = 0; i < n; i++) 
+    {
+        // Longest odd length palindrome with center points as i.
+        int len1 = expandAroundCenter(str, i, i);
+
+        // Longest even length palindrome with center points as i and i + 1.
+        int len2 = expandAroundCenter(str, i, i + 1);
+                
+        int len = max(len1,len2);
+        
+        
+        // Update the start and end. ?? how
+        if( len > end - start + 1){
+            start = i - (len - 1)/2;
+            end = i + (len)/2;
+            }
+        }
+        
+        return str.substr(start, end - start + 1);
+    }
+};
