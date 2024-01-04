c1c478741aad7de126245e05546d03f8c1aa6d3b
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2024-01-04 12:44:56 +0530

    Time: 536 ms (98.18%), Space: 32.4 MB (6.06%) - LeetHub

---
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        counter = Counter(nums)
+        ans = 0
+        for c in counter.values():
+            if c == 1: 
+                return -1
+            ans += ceil(c / 3)
+        return ans
