edac0be3f7bd5244a2b0b9d269df63555a8b285d
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2024-03-16 09:31:18 +0530

    Time: 630 ms (34.40%), Space: 22.2 MB (33.26%) - LeetHub

---
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -0,0 +1,36 @@
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+
+        partial_sum = 0
+        
+		# table is a dictionary
+		# key : partial sum value
+		# value : the left-most index who has the partial sum value
+		
+        table = { 0: -1}
+        
+        max_length = 0
+        
+        for idx, number in enumerate( nums ):
+            
+            # partial_sum add 1 for 1
+            # partial_sum minus 1 for 0
+            
+            if number:
+                partial_sum += 1
+            else:
+                partial_sum -= 1
+                
+            
+            if partial_sum in table:
+                
+                # we have a subarray with equal number of 0 and 1
+                # update max length
+                
+                max_length = max( max_length, ( idx - table[partial_sum] ) )
+                
+            else:
+                # update the left-most index for specified partial sum value
+                table[ partial_sum ] = idx
+                
+        return max_length
