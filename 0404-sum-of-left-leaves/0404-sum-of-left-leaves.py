1613e4b7e7dfee9dd6a0687f598dbe893670ec21
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2024-04-14 07:36:42 +0530

    Time: 39 ms (36.80%), Space: 16.8 MB (47.79%) - LeetHub

---
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
@@ -0,0 +1,25 @@
+class Solution:
+    
+    def sumOfLeftLeaves(self, root: TreeNode) -> int:
+        
+        if root is None: 
+            return 0
+
+        def is_leaf(node):
+            return node is not None and node.left is None and node.right is None
+
+        stack = [root]
+        total = 0
+        while stack:
+            sub_root = stack.pop()
+            # Check if the left node is a leaf node.
+            if is_leaf(sub_root.left):
+                total += sub_root.left.val
+            # If the right node exists, put it on the stack.
+            if sub_root.right is not None:
+                stack.append(sub_root.right)
+            # If the left node exists, put it on the stack.
+            if sub_root.left is not None:
+                stack.append(sub_root.left)
+
+        return total
