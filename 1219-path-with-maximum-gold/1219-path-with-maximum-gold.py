4877c0cb530cfedbeaf7269567bc9a3e101241e2
Aman Ali <amanali.siddiqui2020@vitstudent.ac.in>
2024-05-14 09:18:27 +0530

    Time: 3543 ms (26.49%), Space: 16.5 MB (94.92%) - LeetHub

---
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.py
@@ -0,0 +1,35 @@
+class Solution:
+    def getMaximumGold(self, grid: List[List[int]]) -> int:
+        DIRECTIONS = [0, 1, 0, -1, 0]
+        rows = len(grid)
+        cols = len(grid[0])
+        max_gold = 0
+
+        def dfs_backtrack(grid, rows, cols, row, col):
+            # Base case: this cell is not in the matrix or has no gold
+            if row < 0 or col < 0 or row == rows or col == cols or \
+                    grid[row][col] == 0:
+                return 0
+            max_gold = 0
+
+            # Mark the cell as visited and save the value
+            original_val = grid[row][col]
+            grid[row][col] = 0
+
+            # Backtrack in each of the four directions
+            for direction in range(4):
+                max_gold = max(max_gold,
+                               dfs_backtrack(grid, rows, cols, 
+                                             DIRECTIONS[direction] + row,
+                                             DIRECTIONS[direction + 1] + col))
+
+            # Set the cell back to its original value
+            grid[row][col] = original_val
+            return max_gold + original_val
+
+        # Search for the path with the maximum gold starting from each cell
+        for row in range(rows):
+            for col in range(cols):
+                max_gold = max(max_gold, dfs_backtrack(grid, rows, cols, row, 
+                                                       col))
+        return max_gold
